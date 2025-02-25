467badbe975142841feb26d540b3bb78c09d19f2
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-02-25 22:18:56 +0530

    Time: 0 ms (100%), Space: 8.3 MB (68.49%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int n=word.length();
+        int i;
+        for(i=0;i<n;i++)
+        {
+            if(word[i]==ch)
+            {
+                reverse(word.begin(),word.begin()+i+1);
+                break;
+            }
+        }
+        return word;
+    }
+};
