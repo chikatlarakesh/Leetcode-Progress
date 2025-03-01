2bb9d73b80ade9ddec100e104a71d9259bee786a
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-03-01 22:08:14 +0530

    Time: 4 ms (4.76%), Space: 13.3 MB (3.12%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        int i;
+        for(i=0;i<n-1;i++)
+        {
+            if(nums[i]==0)
+            {
+                continue;
+            }
+            else if(nums[i]==nums[i+1])
+            {
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        int j =0;
+        for(int i =0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        return nums;
+    }
+};
