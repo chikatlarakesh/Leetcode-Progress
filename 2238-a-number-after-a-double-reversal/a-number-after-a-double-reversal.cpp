b1ef096cc804c9e3cfe898aa0c01d4f8ce4d4b46
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-03-15 21:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (29.07%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        // int i;
-        // int val=num;
-        // for(i=0;i<2;i++)
-        // {
-        //     string s=to_string(val);
-        //     reverse(s.begin(),s.end());
-        //     int val=stoi(s);
-        // }
-        // if(val==num)
-        // {
-        //     return true;
-        // }
-        // return false;
         if(num==0)
         {
             return true;
