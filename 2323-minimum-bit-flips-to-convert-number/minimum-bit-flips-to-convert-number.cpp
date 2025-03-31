d668f9bc5ccea9c1ef4b47f1184f367c11eba358
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-03-31 20:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (60.04%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -3,12 +3,17 @@ public:
     int minBitFlips(int start, int goal) {
         int result=start^goal;
         int count=0;
-        for(int i=0;i<31;i++)
+        // for(int i=0;i<31;i++)
+        // {
+        //     if(result&(1<<i))
+        //     {
+        //         count++;
+        //     }
+        // }
+        while(result)
         {
-            if(result&(1<<i))
-            {
-                count++;
-            }
+            result=(result&result-1);
+            count++;
         }
         return count;
     }
