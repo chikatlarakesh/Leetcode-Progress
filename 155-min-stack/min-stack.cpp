674c60fcdc0874f96c161fd05db21d3a89809c75
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-04-25 13:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (30.64%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,6 +1,7 @@
 class MinStack {
 public:
-    stack<pair<int,int>> st;
+    stack<long long> st;
+    long long min=INT_MAX;
     MinStack() {
         
     }
@@ -8,65 +9,105 @@ public:
     void push(int val) {
         if(st.empty())
         {
-            st.push({val,val});
+            st.push(val);
+            min=val;
         }
-        else 
+        else
         {
-            st.push({val,min(val,st.top().second)});
+            if(val<min)
+            {
+                long long x=2*(long long)val-min;
+                st.push(x);
+                min=val;
+            }
+            else st.push(val);
         }
     }
     
     void pop() {
+        if(st.top()<min)
+        {
+            min=2*min-st.top();
+        }
         st.pop();
     }
     
     int top() {
-        return st.top().first;
+        if(st.top()<min)
+        {
+            return min;
+        }
+        return st.top();
     }
     
     int getMin() {
-        return st.top().second;
+        return min;
     }
 
-
-
-
-    // stack<int> s;
+    //stack<pair<int,int>> st;
     // MinStack() {
         
     // }
     
     // void push(int val) {
-    //     s.push(val);
+    //     if(st.empty())
+    //     {
+    //         st.push({val,val});
+    //     }
+    //     else 
+    //     {
+    //         st.push({val,min(val,st.top().second)});
+    //     }
     // }
     
     // void pop() {
-    //     s.pop();
+    //     st.pop();
     // }
     
     // int top() {
-    //     return s.top();
+    //     return st.top().first;
     // }
     
     // int getMin() {
-    //     int min=INT_MAX;
-    //     stack<int> s1;
-    //     while(!s.empty())
-    //     {
-    //         if(s.top()<min)
-    //         {
-    //             min=s.top();
-    //         }
-    //         s1.push(s.top());
-    //         s.pop();
-    //     }
-    //     while(!s1.empty())
-    //     {
-    //         s.push(s1.top());
-    //         s1.pop();
-    //     }
-    //     return min;
+    //     return st.top().second;
     // }
+
+    // // stack<int> s;
+    // // MinStack() {
+        
+    // // }
+    
+    // // void push(int val) {
+    // //     s.push(val);
+    // // }
+    
+    // // void pop() {
+    // //     s.pop();
+    // // }
+    
+    // // int top() {
+    // //     return s.top();
+    // // }
+    
+    // // int getMin() {
+    // //     int min=INT_MAX;
+    // //     stack<int> s1;
+    // //     while(!s.empty())
+    // //     {
+    // //         if(s.top()<min)
+    // //         {
+    // //             min=s.top();
+    // //         }
+    // //         s1.push(s.top());
+    // //         s.pop();
+    // //     }
+    // //     while(!s1.empty())
+    // //     {
+    // //         s.push(s1.top());
+    // //         s1.pop();
+    // //     }
+    // //     return min;
+    // // }
 };
 
 /**
