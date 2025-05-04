00ee3d6f831df4ea7e983ba4cb61b6d6552bed07
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-04 19:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (19.33%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int> prefixMax(n);
-        vector<int> suffixMax(n);
-        int leftMax=0,rightMax=0;
-        prefixMax[0]=height[0];
-        suffixMax[n-1]=height[n-1];
+        vector<int> leftSuffix(n);
+        vector<int> rightSuffix(n);
+        leftSuffix[0]=height[0];
+        rightSuffix[n-1]=height[n-1];
         for(int i=1;i<n;i++)
         {
-            prefixMax[i]=max(prefixMax[i-1],height[i]);
+            leftSuffix[i]=max(leftSuffix[i-1],height[i]);
         }
         for(int i=n-2;i>=0;i--)
         {
-            suffixMax[i]=max(suffixMax[i+1],height[i]);
+            rightSuffix[i]=max(rightSuffix[i+1],height[i]);
         }
-        int result=0;
+        int units=0;
         for(int i=0;i<n;i++)
         {
-            leftMax=prefixMax[i];
-            rightMax=suffixMax[i];
-            result+=min(leftMax,rightMax)-height[i];
+            int small=min(leftSuffix[i],rightSuffix[i]);
+            units+=small-height[i];
         }
-        return result;
+        return units;
     }
 };
