0feaa1374f3ff964b9de825f648e9d2e3211fe26
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-08 11:56:36 +0530

    Time: 39 ms (32.06%) | Memory: 92.1 MB (59.58%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,22 +1,30 @@
 class StockSpanner {
 public:
-    vector<int> result;
+    //vector<int> result;
+    stack<pair<int,int>> st;
+    int count=0,span;
     StockSpanner() {
         
     }
     
     int next(int price) {
-        int count=1;
-        int n=result.size();
-        int i=n-1;
-        while(!result.empty() && i>=0)
-        {
-            if(result[i]<=price) count++;
-            else break;
-            i--;
-        }
-        result.push_back(price);
-        return count;
+        while(!st.empty() && st.top().first<=price) st.pop();
+        if(st.empty()) span=count+1;
+        else span=count-st.top().second;
+        st.push({price,count});
+        count++;
+        return span;
+        // int count=1;
+        // int n=result.size();
+        // int i=n-1;
+        // while(!result.empty() && i>=0)
+        // {
+        //     if(result[i]<=price) count++;
+        //     else break;
+        //     i--;
+        // }
+        // result.push_back(price);
+        // return count;
     }
 };
 
