ae707e23a8e03bbea5f021b4bf91082a90dbf45f
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-08 11:06:42 +0530

    Time: 2 ms (43.17%) | Memory: 111.3 MB (74.11%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -4,20 +4,29 @@ public:
         int n=nums.size();
         int i=0;
         long long count=0,sum=0;
-        while(i<n)
+        for(int num: nums)
         {
-            while(i<n && nums[i]!=0)
+            if(num==0)
             {
-                i++;
-            }
-            count=0;
-            while(i<n && nums[i]==0)
-            {
-                i++;
                 count++;
+                sum+=count;
             }
-            sum+=(count*(count+1))/2;
+            else count=0;
         }
+        // while(i<n)
+        // {
+        //     while(i<n && nums[i]!=0)
+        //     {
+        //         i++;
+        //     }
+        //     count=0;
+        //     while(i<n && nums[i]==0)
+        //     {
+        //         i++;
+        //         count++;
+        //     }
+        //     sum+=(count*(count+1))/2;
+        // }
         return sum;
     }
 };
