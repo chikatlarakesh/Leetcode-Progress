5542a7a1c86d4a806a4f02c56522ed31d91e8ae7
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-12 21:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.27%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int> mp;
+        unordered_set<char> letters;
+        stack<char> st;
+        for(char ch:s)
+        {
+            mp[ch]++;
+        }
+        for(char ch: s)
+        {
+            mp[ch]--;
+            if(letters.count(ch)) continue;
+            while(!st.empty() && st.top()>ch && mp[st.top()]>0)
+            {
+                letters.erase(st.top());
+                st.pop();
+            }
+            st.push(ch);
+            letters.insert(ch);
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
