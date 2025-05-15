e47a8f6fef33bd7c24b297add76ababd27d1e4f2
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-15 11:22:50 +0530

    Time: 47 ms (24.49%) | Memory: 32.4 MB (36.98%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n%k!=0) return false;
+        map<int,int> m;
+        for(int num:nums) m[num]++;
+        while(!m.empty())
+        {
+            int start=m.begin()->first;
+            for(int i=0;i<k;i++)
+            {
+                if(m[start+i]==0) return false;
+                m[start+i]--;
+                if(m[start+i]==0) m.erase(start+i);
+                
+            }
+        }
+        return true;
+    }
+};
