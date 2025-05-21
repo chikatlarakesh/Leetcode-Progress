8afe60a9d57d0a3d91a04e17fa205acd31576948
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-21 09:58:30 +0530

    Time: 75 ms (70.61%) | Memory: 37.8 MB (64.02%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -1,57 +1,38 @@
-class Node{
-    public:
-    int data;
-    int row;
-    int col;
-
-    Node(int data1,int row1,int col1)
-    {
-        data=data1;
-        row=row1;
-        col=col1;
-    }
-};
-
-class compare{
-    public:
-    bool operator()(Node* a,Node* b)
-    {
-        return a->data>b->data;
-    }
-};
-
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<Node*,vector<Node*>,compare> pq;
         int k=nums.size();
-        int i,n;
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>> pq;
         int mini=INT_MAX,maxi=INT_MIN;
-        for(i=0;i<k;i++)
+        for(int i=0;i<k;i++)
         {
             int val=nums[i][0];
             mini=min(mini,val);
             maxi=max(maxi,val);
-            pq.push(new Node(val,i,0));
+            pq.push({val,i,0});
         }
+
         int start=mini,end=maxi;
         while(!pq.empty())
         {
-            Node* temp=pq.top();
+            auto top=pq.top();
             pq.pop();
+            int val=get<0>(top);
+            int row=get<1>(top);
+            int col=get<2>(top);
+            mini=val;
 
-            mini=temp->data;
             if(maxi-mini<end-start)
             {
                 start=mini;
                 end=maxi;
             }
-            int currListLen=nums[temp->row].size();
-            if(temp->col+1<currListLen)
+            int n=nums[row].size();
+            if(col+1<n)
             {
-                int val=nums[temp->row][temp->col+1];
+                int val=nums[row][col+1];
                 maxi=max(maxi,val);
-                pq.push(new Node(val,temp->row,temp->col+1));
+                pq.push({val,row,col+1});
             }
             else break;
         }
