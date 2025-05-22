57dd013456c40f6cda5ca337747910646f16f675
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-22 10:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (78.87%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,28 +4,78 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         int i,j;
-        vector<int> row(n,0);
-        vector<int> col(m,0);
+        int col0=1;
         for(i=0;i<n;i++)
         {
             for(j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    if(j!=0)
+                    {
+                        matrix[0][j]=0;
+                    }
+                    else col0=0;
                 }
             }
         }
-        for(i=0;i<n;i++)
+
+        for(i=1;i<n;i++)
+        {
+            for(j=1;j<m;j++)
+            {
+                if(matrix[i][j]!=0)
+                {
+                    if(matrix[i][0]==0 || matrix[0][j]==0)
+                    {
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        
+        if(matrix[0][0]==0)
         {
             for(j=0;j<m;j++)
             {
-                if(row[i]==1 || col[j]==1)
-                {
-                    matrix[i][j]=0;
-                }
+                matrix[0][j]=0;
             }
         }
+        if(col0==0)
+        {
+            for(i=0;i<n;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+
+
+
+
+
+        // vector<int> row(n,0);
+        // vector<int> col(m,0);
+        // for(i=0;i<n;i++)
+        // {
+        //     for(j=0;j<m;j++)
+        //     {
+        //         if(matrix[i][j]==0)
+        //         {
+        //             row[i]=1;
+        //             col[j]=1;
+        //         }
+        //     }
+        // }
+        // for(i=0;i<n;i++)
+        // {
+        //     for(j=0;j<m;j++)
+        //     {
+        //         if(row[i]==1 || col[j]==1)
+        //         {
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
     }
 };
