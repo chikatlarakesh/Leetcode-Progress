a9d297f182b83339ed7846753f88a34abc20cb2e
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-05-22 08:39:09 +0530

    Time: 3 ms (11.51%) | Memory: 18.6 MB (78.87%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int i,j;
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(row[i]==1 || col[j]==1)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+};
