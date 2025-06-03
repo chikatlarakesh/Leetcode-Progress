1ff6810f026b1abe62b4665ed329545b62daed98
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-03 16:27:50 +0530

    Time: 12 ms (44.36%) | Memory: 11 MB (80.01%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -2,17 +2,30 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n=s.length();
-        vector<int> result(3,-1);
-        int substrings=0;
+        int a=-1,b=-1,c=-1,substrings=0;
         for(int i=0;i<n;i++)
         {
-            int ind=s[i]-'a';
-            result[ind]=i;
-            if(result[0]!=-1 && result[1]!=-1 && result[2]!=-1)
+            if(s[i]=='a') a=i;
+            else if(s[i]=='b') b=i;
+            else c=i;
+            if(a!=-1 && b!=-1 && c!=-1)
             {
-                substrings+=1+min({result[0],result[1],result[2]});
+                substrings+=1+min({a,b,c});
             }
         }
         return substrings;
+        
+        // //vector<int> result(3,-1);
+        // int substrings=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     //int ind=s[i]-'a';
+        //     //result[ind]=i;
+        //     // if(result[0]!=-1 && result[1]!=-1 && result[2]!=-1)
+        //     // {
+        //     //     substrings+=1+min({result[0],result[1],result[2]});
+        //     // }
+        // }
+        // return substrings;
     }
 };
