845e36dede4779a72283e3d9177d3f3117e718d2
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-05 09:46:11 +0530

    Time: 2 ms (58.57%) | Memory: 32.7 MB (40.83%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int atMost(vector<int>& nums, int goal)
+    {
+        if(goal<0) return 0;
         int n=nums.size();
-        int totalSubarrays=0,prefixSum=0;
-        unordered_map<int,int> mp;
-        mp[0]=1;
-        for(int i=0;i<n;i++)
+        int l=0,count=0,sum=0;
+        for(int r=0;r<n;r++)
         {
-            prefixSum+=nums[i];
-            int complement=prefixSum-goal;
-            if(mp.find(complement)!=mp.end())
+            sum+=nums[r];
+            while(sum>goal)
             {
-                totalSubarrays+=mp[complement];
+                sum-=nums[l];
+                l++;
             }
-            mp[prefixSum]++;
+            count+=r-l+1;
         }
-        return totalSubarrays;
+        return count;
+    }
+
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atMost(nums,goal)-atMost(nums,goal-1);
+
+        // int totalSubarrays=0,prefixSum=0;
+        // unordered_map<int,int> mp;
+        // mp[0]=1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     prefixSum+=nums[i];
+        //     int complement=prefixSum-goal;
+        //     if(mp.find(complement)!=mp.end())
+        //     {
+        //         totalSubarrays+=mp[complement];
+        //     }
+        //     mp[prefixSum]++;
+        // }
+        // return totalSubarrays;
     }
 };
