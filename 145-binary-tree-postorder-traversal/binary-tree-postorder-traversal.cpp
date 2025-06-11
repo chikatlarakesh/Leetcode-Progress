3bacdfc434a75f771504670fa43e57c04b51794d
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-11 10:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (71.73%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -20,29 +20,65 @@ public:
     // }
 
     vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if(root==NULL) return result;
+        stack<TreeNode*> st;
+        TreeNode* curr=root;
+        TreeNode* temp=nullptr;
+
+        while(curr!=NULL || !st.empty())
+        {
+            if(curr!=NULL)
+            {
+                st.push(curr);
+                curr=curr->left;
+            }
+            else
+            {
+                temp=st.top()->right;
+                if(temp==NULL)
+                {
+                    temp=st.top();
+                    st.pop();
+                    result.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right)
+                    {
+                        temp=st.top();
+                        st.pop();
+                        result.push_back(temp->val);
+                    }
+                }
+                else curr=temp;
+            }
+        }
+        return result;
+
         // vector<int> result;
         // postOrder(root,result);
         // return result;
-        vector<int> result;
-        if(root==NULL) return result;
-        stack<TreeNode*> s1;
-        s1.push(root);
-        stack<TreeNode*> s2;
-        TreeNode* node=root;
-        while(true)
-        {
-            if(s1.empty()) break;
-            node=s1.top();
-            s1.pop();
-            s2.push(node);
-            if(node->left) s1.push(node->left);
-            if(node->right) s1.push(node->right);
-        }
-        while(!s2.empty())
-        {
-            result.push_back(s2.top()->val);
-            s2.pop();
-        }
-        return result;
+
+
+
+        // vector<int> result;
+        // if(root==NULL) return result;
+        // stack<TreeNode*> s1;
+        // s1.push(root);
+        // stack<TreeNode*> s2;
+        // TreeNode* node=root;
+        // while(true)
+        // {
+        //     if(s1.empty()) break;
+        //     node=s1.top();
+        //     s1.pop();
+        //     s2.push(node);
+        //     if(node->left) s1.push(node->left);
+        //     if(node->right) s1.push(node->right);
+        // }
+        // while(!s2.empty())
+        // {
+        //     result.push_back(s2.top()->val);
+        //     s2.pop();
+        // }
+        // return result;
     }
 };
