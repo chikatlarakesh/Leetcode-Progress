da0ed5e103c00abe36779d12b7d51c9f909d7412
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-12 18:40:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (45.58%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -13,31 +13,33 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root==NULL) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int count=1;
-        bool present=false;
-        while(!q.empty())
-        {
-            int n=q.size();
-            present=false;
-            for(int i=0;i<n;i++)
-            {
-                TreeNode* temp=q.front();
-                if(temp->left) 
-                {
-                    q.push(temp->left);
-                    present=true;
-                }
-                if(temp->right)
-                {
-                    q.push(temp->right);
-                    present=true;
-                }
-                q.pop();
-            }
-            if(present) count++;
-        }
-        return count;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+        // if(root==NULL) return 0;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // int count=1;
+        // bool present=false;
+        // while(!q.empty())
+        // {
+        //     int n=q.size();
+        //     present=false;
+        //     for(int i=0;i<n;i++)
+        //     {
+        //         TreeNode* temp=q.front();
+        //         if(temp->left) 
+        //         {
+        //             q.push(temp->left);
+        //             present=true;
+        //         }
+        //         if(temp->right)
+        //         {
+        //             q.push(temp->right);
+        //             present=true;
+        //         }
+        //         q.pop();
+        //     }
+        //     if(present) count++;
+        // }
+        // return count;
     }
 };
