e8159e7a15ac55c180d0d8ff74da014fa04dd28c
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-15 22:53:56 +0530

    Time: 2 ms (21.80%) | Memory: 8.1 MB (93.84%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int maxDiff(int num) {
+        string number=to_string(num);
+        int n=number.length();
+        char smallStart=number[0];
+        char largeStart=number[0];
+        char largest=number[0],smallest=number[0];
+        int smallIndex=0;
+        string maximum="";
+        string minimum="";
+        if(largeStart=='9')
+        {
+            for(char ch:number)
+            {
+                if(ch!='9')
+                {
+                    largest=ch;
+                    break;
+                }
+            }
+        }
+        if(smallStart=='1')
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(number[i]!='1' && number[i]!='0')
+                {
+                    smallIndex=i;
+                    smallest=number[i];
+                    break;
+                }
+            }
+        }
+        if(largest!='9')
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(number[i]==largest)
+                {
+                    maximum+='9';
+                }
+                else maximum+=number[i];
+            }
+        }
+        else maximum=number;
+        if(smallIndex==0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(number[i]==smallest)
+                {
+                    minimum+='1';
+                }
+                else minimum+=number[i];
+            }
+        }
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(number[i]==smallest)
+                {
+                    minimum+='0';
+                }
+                else minimum+=number[i];
+            }
+        }
+        int maxi=stoi(maximum);
+        int mini=stoi(minimum);
+        return maxi-mini;
+    }
+};
