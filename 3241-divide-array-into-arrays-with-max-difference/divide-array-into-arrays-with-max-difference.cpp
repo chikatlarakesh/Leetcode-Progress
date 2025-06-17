d0a66ee473cd236cec37466c238441e2f3676726
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-17 09:32:16 +0530

    Time: 51 ms (49.14%) | Memory: 120.1 MB (43.98%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -4,22 +4,10 @@ public:
         vector<vector<int>> result;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        int l=0,r=2;
-        while(r<n)
+        for(int i=0;i<n;i+=3)
         {
-            if(nums[r]-nums[l]>k) return result;
-            l+=3;
-            r+=3;
-        }
-        int m=n/3;
-        for(int i=0;i<m;i++)
-        {
-            vector<int> vec;
-            for(int j=i*3;j<i*3+3;j++)
-            {
-                vec.push_back(nums[j]);
-            }
-            result.push_back(vec);
+            if(nums[i+2]-nums[i]>k) return {};
+            result.push_back({nums[i],nums[i+1],nums[i+2]});
         }
         return result;
     }
