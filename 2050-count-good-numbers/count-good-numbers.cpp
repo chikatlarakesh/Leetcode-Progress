fbd91c2b13da466cae37058eed93900752c23d8a
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-06-28 14:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (3.22%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -4,17 +4,14 @@ public:
     long long calculatePower(long long x,long long y)
     {
         if(y==0) return 1;
-        long long ans=calculatePower(x,y/2);
-        ans*=ans;
-        ans%=MOD;
-        if(y%2!=0) ans*=x;
-        ans%=MOD;
+        long long half=calculatePower(x,y/2);
+        long long ans=(half*half)%MOD;
+        if(y%2==1) ans=(ans*x)%MOD;
         return ans;
     }
 
     int countGoodNumbers(long long n) {
-        long long even=n/2;
-        if(n%2!=0) even++;
+        long long even=(n+1)/2;
         long long odd=n/2;
         return (calculatePower(5,even)*calculatePower(4,odd))%MOD;
     }
