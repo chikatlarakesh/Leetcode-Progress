05f492d529175f7f9b2912f93ebbd40e908e0a16
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-01 09:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (83.47%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n=word.length();
+        int count=1;
+        for(int i=1;i<n;i++)
+        {
+            if(word[i]==word[i-1]) count++;
+        }
+        return count;
+    }
+};
