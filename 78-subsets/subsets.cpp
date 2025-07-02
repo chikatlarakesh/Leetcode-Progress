7f3180a788870365009c92981564ce8996a0b38c
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-02 09:25:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.95%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    // void posSSets(int i,vector<int>& nums,vector<int>& res,vector<vector<int>> &result)
-    // {
-    //     int n=nums.size();
-    //     if(i==n)
-    //     {
-    //         result.push_back(res);
-    //         return;
-    //     }
-    //     res.push_back(nums[i]);
-    //     posSSets(i+1,nums,res,result);
-    //     res.pop_back();
-    //     posSSets(i+1,nums,res,result);
-    // }
+    void possSubsets(vector<int>& nums,int n,int i,vector<int>& vec,vector<vector<int>>& result)
+    {
+        if(i==n)
+        {
+            result.push_back(vec);
+            return;
+        }
+        vec.push_back(nums[i]);
+        possSubsets(nums,n,i+1,vec,result);
+        vec.pop_back();
+        possSubsets(nums,n,i+1,vec,result);
+    }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        // posSSets(0,nums,res,result);
         int n=nums.size();
-        int subSets=1<<n;
-        for(int num=0;num<subSets;num++)
-        {
-            vector<int> res;
-            for(int j=0;j<n;j++)
-            {
-                if(num&(1<<j))
-                {
-                    res.push_back(nums[j]);
-                }
-            }
-            result.push_back(res);
-        }
+        vector<int> vec;
+        vector<vector<int>> result;
+        possSubsets(nums,n,0,vec,result);
         return result;
     }
 };
