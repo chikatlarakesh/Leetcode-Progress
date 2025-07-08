dd7e37ea488b6a8cc3e44c68c85baa50e72a3567
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-08 07:37:06 +0530

    Time: 63 ms (54.30%) | Memory: 75.1 MB (39.16%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
+        int n = events.size();
         priority_queue<int,vector<int>,greater<int>> pq;
         sort(events.begin(),events.end());
-        int count=0;
-        int start=events[0][0];
-        int end=0;
-        for(int i=0;i<events.size();i++)
+
+        int day = events[0][0];
+        int i = 0,maxEvents = 0;
+        while(!pq.empty() || i < n)
         {
-            end=max(end,events[i][1]);
-        }
-        int j=0;
-        for(int i=start;i<=end;i++)
-        {
-            while(j<events.size())
+            while(i<n && events[i][0]<=day)
             {
-                if(i==events[j][0])
-                {
-                    pq.push(events[j][1]);
-                    j++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            while(!pq.empty()&&pq.top()<i)
-            {
-                pq.pop();
+                pq.push(events[i][1]);
+                i++;
             }
             if(!pq.empty())
             {
                 pq.pop();
-                count+=1;
+                maxEvents++;
+            }
+            day++;
+
+            while(!pq.empty() && day>pq.top())
+            {
+                pq.pop();
             }
         }
-        return count;
+        return maxEvents;
     }
 };
