fe1a97a12ad689e9699c79125566764593841cf6
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-15 11:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (20.38%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int helper(int n,vector<int>& dp)
+    {
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    }
+
+    int fib(int n) {
+        if(n<=1) return n;
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        return helper(n,dp);
+    }
+};
