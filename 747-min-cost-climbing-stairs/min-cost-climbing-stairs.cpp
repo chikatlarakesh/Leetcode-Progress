dcc5b7bfdf021fd16a13561e1cad7db2d98c887f
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-16 19:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (89.62%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int backtrack(int i,vector<int>& cost,vector<int>& dp)
-    {
-        if(i>=cost.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int left=cost[i]+backtrack(i+1,cost,dp);
-        int right=cost[i]+backtrack(i+2,cost,dp);
-        return dp[i]=min(left,right);
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        for(int i=2;i<n;i++)
+        {
+            cost[i]=cost[i]+min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[n-1],cost[n-2]);
     }
 
-    int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(),-1);
-        int first=backtrack(0,cost,dp);
-        int second=backtrack(1,cost,dp);
-        return min(first,second);
-    }
+    // int backtrack(int i,vector<int>& cost,vector<int>& dp)
+    // {
+    //     if(i>=cost.size()) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int left=cost[i]+backtrack(i+1,cost,dp);
+    //     int right=cost[i]+backtrack(i+2,cost,dp);
+    //     return dp[i]=min(left,right);
+    // }
+
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     vector<int> dp(cost.size(),-1);
+    //     int first=backtrack(0,cost,dp);
+    //     int second=backtrack(1,cost,dp);
+    //     return min(first,second);
+    // }
 };
