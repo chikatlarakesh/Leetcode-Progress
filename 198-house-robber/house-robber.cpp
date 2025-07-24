a83ebf8ae828fd7e706145ff3d063284980c9372
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-24 20:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.26%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int maximumAmount(int i,vector<int>& nums,vector<int>& dp)
-    {
-        if(i<0) return 0;
-        if(i==0) return nums[0];
-        if(dp[i]!=-1) return dp[i];
+    // int maximumAmount(int i,vector<int>& nums,vector<int>& dp)
+    // {
+    //     if(i<0) return 0;
+    //     if(i==0) return nums[0];
+    //     if(dp[i]!=-1) return dp[i];
 
-        int take=maximumAmount(i-2,nums,dp)+nums[i];
-        int notTake=maximumAmount(i-1,nums,dp);
-        return dp[i]=max(take,notTake);
-    }
+    //     int take=maximumAmount(i-2,nums,dp)+nums[i];
+    //     int notTake=maximumAmount(i-1,nums,dp);
+    //     return dp[i]=max(take,notTake);
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int> dp(n,-1);
+    //     return maximumAmount(n-1,nums,dp);
+    // }
 
     int rob(vector<int>& nums) {
         int n=nums.size();
+        if(n==1) return nums[0];
         vector<int> dp(n,-1);
-        return maximumAmount(n-1,nums,dp);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++)
+        {
+            int take=dp[i-2]+nums[i];
+            int notTake=dp[i-1];
+            dp[i]=max(take,notTake);
+        }
+        return dp[n-1];
     }
 };
