1d2fc1a2e60b1426a62130207a732844178672f3
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-24 20:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (80.09%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+        //  MEMOIZATION
+
     // int maximumAmount(int i,vector<int>& nums,vector<int>& dp)
     // {
     //     if(i<0) return 0;
@@ -17,19 +19,38 @@ public:
     //     return maximumAmount(n-1,nums,dp);
     // }
 
+    //TABULATION
+    // int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     if(n==1) return nums[0];
+    //     vector<int> dp(n,-1);
+    //     dp[0]=nums[0];
+    //     dp[1]=max(nums[0],nums[1]);
+
+    //     for(int i=2;i<n;i++)
+    //     {
+    //         int take=dp[i-2]+nums[i];
+    //         int notTake=dp[i-1];
+    //         dp[i]=max(take,notTake);
+    //     }
+    //     return dp[n-1];
+    // }
+
+    //SPACE OPTIMIZATION
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> dp(n,-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        int prev1=nums[0];
+        int prev=max(nums[0],nums[1]);
 
         for(int i=2;i<n;i++)
         {
-            int take=dp[i-2]+nums[i];
-            int notTake=dp[i-1];
-            dp[i]=max(take,notTake);
+            int take=prev1+nums[i];
+            int notTake=prev;
+            int curr=max(take,notTake);
+            prev1=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
     }
 };
