48f6193fce318d7e0f13498c851ddd10a6b4a011
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-25 22:13:33 +0530

    Time: 3 ms (8.11%) | Memory: 9.5 MB (8.74%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int dfs(int row,int col,int rowSize,int colSize,vector<vector<int>>& matrix,vector<vector<int>>& dp)
+    int possiblePaths(int i,int j,vector<vector<int>>& dp)
     {
-        if(row>=rowSize || col>=colSize || matrix[row][col]==-1) return 0;
-        if(row==rowSize-1 && col==colSize-1) return 1;
-        if(dp[row][col]!=-1) return dp[row][col];
-        matrix[row][col]=-1;
-        int uniquePaths=dfs(row,col+1,rowSize,colSize,matrix,dp)+dfs(row+1,col,rowSize,colSize,matrix,dp);
-        matrix[row][col]=0;
-        return dp[row][col]=uniquePaths;
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int top=possiblePaths(i-1,j,dp);
+        int left=possiblePaths(i,j-1,dp);
+        return dp[i][j]=top+left;
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> matrix(m,vector<int>(n,0));
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return dfs(0,0,m,n,matrix,dp);
+        return possiblePaths(m-1,n-1,dp);
     }
 };
