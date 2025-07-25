ba2d5bbf6d5326bda33cb24ce49e0666fe2974ce
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-25 23:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.21%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,7 +5,7 @@ public:
         if(i<0 || j<0 || arr[i][j]==1) return 0;
         if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        
+
         int top=0,left=0;
         top=possiblePaths(i-1,j,arr,dp);
         left=possiblePaths(i,j-1,arr,dp);
@@ -16,25 +16,25 @@ public:
         int n=arr.size();
         int m=arr[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return possiblePaths(n-1,m-1,arr,dp);
+        //return possiblePaths(n-1,m-1,arr,dp);
         // for(int i=0;i<n;i++) dp[i][0]=1;
         // for(int j=0;j<m;j++) dp[0][j]=1;
 
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<m;j++)
-        //     {
-        //         if(arr[i][j]==1) dp[i][j]=0;
-        //         else if(i==0 || j==0) dp[i][j]=1;
-        //         else
-        //         {
-        //             int top=0,left=0;
-        //             if(i>0) top=dp[i-1][j];
-        //             if(j>0) left=dp[i][j-1];
-        //             dp[i][j]=top+left;
-        //         }
-        //     }
-        // }
-        // return dp[n-1][m-1];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(arr[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else
+                {
+                    int top=0,left=0;
+                    if(i>0) top=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=top+left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
