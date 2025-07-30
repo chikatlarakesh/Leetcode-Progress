cc7696cca36297fde2cc1a2b6c5d9ee704821665
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-30 19:38:27 +0530

    Time: 3 ms (87.71%) | Memory: 15.6 MB (42.27%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
-    int targetSum(int i,int sum,vector<int>& nums,int target)
-    {
-        if(i==0)
+//     int partitionArray(int i,int target,vector<int>& arr,vector<vector<int>>& dp)
+//   {
+//       if(i==0)
+//       {
+//           if(target==0 && arr[i]==0) return 2;
+//           if(target==0 || target==arr[i]) return 1;
+//           return 0;
+//       }
+//       if(dp[i][target]!=-1) return dp[i][target];
+      
+//       int take=0;
+//       if(target>=arr[i]) take=partitionArray(i-1,target-arr[i],arr,dp);
+//       int notTake=partitionArray(i-1,target,arr,dp);
+//       return dp[i][target]=take+notTake;
+//   }
+  
+    int countPartitions(vector<int>& arr, int d) {
+        int n=arr.size();
+        int sum=accumulate(arr.begin(),arr.end(),0);
+        if((d+sum)%2==1 || (d+sum)<0) return 0;
+        int target=(d+sum)/2;
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
+        //if(target%2==1) return 0;
+        for(int i=0;i<n;i++)
         {
-            int add=sum+nums[i];
-            int subtract=sum-nums[i];
-            if(add==target && subtract==target) return 2;
-            if(add==target || subtract==target) return 1;
-            return 0;
+            for(int j=0;j<=target;j++)
+            {
+                if(i==0)
+                {
+                    if(j==0 && arr[i]==0) dp[i][j]=2;
+                    else if(j==0 || j==arr[i]) dp[i][j]=1;
+                    else dp[i][j]=0;
+                }
+                else
+                {
+                     int take=0;
+                      if(j>=arr[i]) take=dp[i-1][j-arr[i]];
+                      int notTake=dp[i-1][j];
+                      dp[i][j]=take+notTake;
+                }
+            }
         }
-
-        int add=targetSum(i-1,sum+nums[i],nums,target);
-        int subtract=targetSum(i-1,sum-nums[i],nums,target);
-        return add+subtract;
+        return dp[n-1][target];
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        return targetSum(n-1,0,nums,target);
+        return countPartitions(nums,target);
     }
 };
