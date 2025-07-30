f9ae4c65e6a4f661b5b682ed5b9394c7c4a61018
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-30 23:19:33 +0530

    Time: 19 ms (55.47%) | Memory: 10.8 MB (75.57%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -25,11 +25,39 @@ public:
     //     return coinChange(n-1,amount,coins,dp);
     // }
 
+    // TABULATION
+    // int change(int amount, vector<int>& coins) {
+    //     if(amount==0) return 1;
+    //     int n=coins.size();
+    //     // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+    //     vector<vector<long long>> dp(n,vector<long long>(amount+1,0));
+        
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<=amount;j++)
+    //         {
+    //             if(i==0)
+    //             {
+    //                 if(j==0 || j%coins[i]==0) dp[i][j]=1;
+    //             }
+    //             else
+    //             {
+    //                 int take=0;
+    //                 if(j>=coins[i]) take=dp[i][j-coins[i]];
+    //                 int notTake=dp[i-1][j];
+    //                 dp[i][j]=(long long)take+notTake;
+    //             }
+    //         }
+    //     }
+    //     return (int)dp[n-1][amount];
+    // }
+
+    // SPACE OPTIMIZATION
     int change(int amount, vector<int>& coins) {
         if(amount==0) return 1;
         int n=coins.size();
-        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        vector<vector<long long>> dp(n,vector<long long>(amount+1,0));
+        vector<long long> prev(amount+1,0);
+        vector<long long> curr(amount+1,0);
         
         for(int i=0;i<n;i++)
         {
@@ -37,17 +65,18 @@ public:
             {
                 if(i==0)
                 {
-                    if(j==0 || j%coins[i]==0) dp[i][j]=1;
+                    if(j==0 || j%coins[i]==0) curr[j]=1;
                 }
                 else
                 {
                     int take=0;
-                    if(j>=coins[i]) take=dp[i][j-coins[i]];
-                    int notTake=dp[i-1][j];
-                    dp[i][j]=(long long)take+notTake;
+                    if(j>=coins[i]) take=curr[j-coins[i]];
+                    int notTake=prev[j];
+                    curr[j]=(long long)take+notTake;
                 }
             }
+            prev=curr;
         }
-        return (int)dp[n-1][amount];
+        return (int)prev[amount];
     }
 };
