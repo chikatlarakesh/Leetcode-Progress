e9639b6bba3b0f4264a3820181ba1d589a441b75
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-30 23:08:22 +0530

    Time: 38 ms (11.66%) | Memory: 46 MB (17.55%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
-    int coinChange(int i,int amount,vector<int>& coins,vector<vector<int>>& dp)
-    {
-        if(i==0)
-        {
-            if(amount==0) return 1;
-            if(amount%coins[i]==0) return 1;
-            return 0;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
 
-        int take=0;
-        if(amount>=coins[i]) take=coinChange(i,amount-coins[i],coins,dp);
-        int notTake=coinChange(i-1,amount,coins,dp);
-        return dp[i][amount]=take+notTake;
-    }
+    // MEMOIZATION
+    // int coinChange(int i,int amount,vector<int>& coins,vector<vector<int>>& dp)
+    // {
+    //     if(i==0)
+    //     {
+    //         if(amount==0) return 1;
+    //         if(amount%coins[i]==0) return 1;
+    //         return 0;
+    //     }
+    //     if(dp[i][amount]!=-1) return dp[i][amount];
+
+    //     int take=0;
+    //     if(amount>=coins[i]) take=coinChange(i,amount-coins[i],coins,dp);
+    //     int notTake=coinChange(i-1,amount,coins,dp);
+    //     return dp[i][amount]=take+notTake;
+    // }
+
+    // int change(int amount, vector<int>& coins) {
+    //     if(amount==0) return 1;
+    //     int n=coins.size();
+    //     vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+    //     return coinChange(n-1,amount,coins,dp);
+    // }
 
     int change(int amount, vector<int>& coins) {
         if(amount==0) return 1;
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return coinChange(n-1,amount,coins,dp);
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        vector<vector<long long>> dp(n,vector<long long>(amount+1,0));
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                if(i==0)
+                {
+                    if(j==0 || j%coins[i]==0) dp[i][j]=1;
+                }
+                else
+                {
+                    int take=0;
+                    if(j>=coins[i]) take=dp[i][j-coins[i]];
+                    int notTake=dp[i-1][j];
+                    dp[i][j]=(long long)take+notTake;
+                }
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
