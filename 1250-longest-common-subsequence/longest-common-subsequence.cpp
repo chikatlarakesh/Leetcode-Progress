95f6f814602647a713be24dddc6da061aa9785c5
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-07-31 19:47:28 +0530

    Time: 21 ms (82.37%) | Memory: 9 MB (96.42%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -19,26 +19,51 @@ public:
     //     return maxLength(n-1,m-1,text1,text2,dp);
     // }
 
+    // int longestCommonSubsequence(string text1, string text2) {
+    //     int n=text1.length();
+    //     int m=text2.length();
+    //     vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+    //     for(int i=0;i<=n;i++)
+    //     {
+    //         for(int j=0;j<=m;j++)
+    //         {
+    //             if(i==0 || j==0) dp[i][j]=0;
+    //             else
+    //             {
+    //                 if(text1[i-1]==text2[j-1])
+    //                 {
+    //                     dp[i][j]=1+dp[i-1][j-1];
+    //                 }
+    //                 else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    //             }
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.length();
         int m=text2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> prev(m+1,-1);
+        vector<int> curr(m+1,-1);
         
         for(int i=0;i<=n;i++)
         {
             for(int j=0;j<=m;j++)
             {
-                if(i==0 || j==0) dp[i][j]=0;
+                if(i==0 || j==0) curr[j]=0;
                 else
                 {
                     if(text1[i-1]==text2[j-1])
                     {
-                        dp[i][j]=1+dp[i-1][j-1];
+                        curr[j]=1+prev[j-1];
                     }
-                    else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    else curr[j]=max(prev[j],curr[j-1]);
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
