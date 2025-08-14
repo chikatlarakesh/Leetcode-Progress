d80b7d9676b71ba217a1eab79256b0b1f5577c18
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-14 19:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (52.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -3,15 +3,13 @@ public:
     string largestGoodInteger(string num) {
         int n=num.length();
         string ans="";
-        char ch='0';
         for(int i=0;i<n-2;i++)
         {
-            if(num[i]>=ch && num[i]==num[i+1] && num[i+1]==num[i+2])
+            if(num[i]==num[i+1] && num[i+1]==num[i+2])
             {
-                ch=num[i];
-                ans="";
-                for(int j=0;j<3;j++) ans+=num[i];
-            }
+                string temp=string(3,num[i]);
+                ans=max(ans,temp);
+            } 
         }
         return ans;
     }
