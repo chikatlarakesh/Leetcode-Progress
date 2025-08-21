93f3d2e075f6349fd7a3ce26c5f525938a4d2601
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-21 23:06:09 +0530

    Time: 2 ms (31.43%) | Memory: 97.3 MB (86.87%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy=INT_MAX,sell=INT_MIN;
-        int maxProfit=0;
+        int buy=prices[0],maxProfit=0;
         for(int num:prices)
         {
-            if(num<buy)
-            {
-                buy=sell=num;
-            }
-            else if(num>sell) sell=num;
-            maxProfit=max(maxProfit,sell-buy);
+            maxProfit=max(maxProfit,num-buy);
+            buy=min(buy,num);
         }
         return maxProfit;
     }
