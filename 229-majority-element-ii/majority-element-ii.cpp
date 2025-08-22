9ac8debb806604d3bebf63e8bfdf5101bffa3aba
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-22 22:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (61.58%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-        int freq=n/3,val=nums[0],count=1;
-        vector<int> result;
+        int ele1=0,cnt1=0,ele2=0,cnt2=0;
 
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            if(val!=nums[i]) 
+            if(cnt1==0 && nums[i]!=ele2)
             {
-                if(count>freq) result.push_back(val);
-                val=nums[i];
-                count=1;
+                ele1=nums[i];
+                cnt1++;
             }
-            else count++;
+            else if(cnt2==0 && nums[i]!=ele1)
+            {
+                ele2=nums[i];
+                cnt2++;
+            }
+            else if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
         }
-        if(count>freq) result.push_back(val);
+
+        vector<int> result;
+        cnt1=0,cnt2=0;
+        int freq=n/3;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+        }
+
+        if(cnt1>freq) result.push_back(ele1);
+        if(cnt2>freq) result.push_back(ele2);
+        sort(result.begin(),result.end());
         return result;
     }
 };
