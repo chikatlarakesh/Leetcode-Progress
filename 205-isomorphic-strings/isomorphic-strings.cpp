100ccf3d119c8ca3e693dde59c75a2b67eba58a7
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-24 12:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.07%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         int n=s.length();
-        vector<int> sMap(256,0),tMap(256,0);
+        vector<int> sMap(256,-1),tMap(256,-1);
 
         for(int i=0;i<n;i++)
         {
-            if((sMap[s[i]]!=0 || tMap[t[i]]!=0) &&
-                (sMap[s[i]]!=t[i]+1 || tMap[t[i]]!=s[i]+1)) return false;
-            sMap[s[i]]=t[i]+1;
-            tMap[t[i]]=s[i]+1;
+            if((sMap[s[i]]!=-1 || tMap[t[i]]!=-1) &&
+                (sMap[s[i]]!=t[i] || tMap[t[i]]!=s[i])) return false;
+            sMap[s[i]]=t[i];
+            tMap[t[i]]=s[i];
         }
         return true;
     }
