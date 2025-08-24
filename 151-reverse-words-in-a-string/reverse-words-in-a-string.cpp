4f12cece4eb9e196f68be87bc94cb256bc306e84
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-24 14:49:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (73.94%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        while(s.back()==' ') s.pop_back();
-        reverse(s.begin(),s.end());
-        while(s.back()==' ') s.pop_back();
-        reverse(s.begin(),s.end());
+        // Reverse the entire string
+        // 1. Find the first character that is not equal to space
+        // 2. Move all the characters to the front filling up the spaces
+        // 3. Reverse the substring and add a space
+        // 4. Repeat the process
 
         int n=s.length();
-        string ans="";
-        int i=n-1;
-        while(i>=0)
+        reverse(s.begin(),s.end());
+        int i=0,j=0,start=0,end=0;
+        while(j<n)
         {
-            string temp="";
-            while(i>=0 && s[i]!=' ')
+            while(j<n && s[j]==' ') j++;
+            start=i;
+            while(j<n && s[j]!=' ')
             {
-                temp+=s[i];
-                i--;
+                s[i]=s[j];
+                i++;
+                j++;
             }
-            while(i>=0 && s[i]==' ') i--;
-            reverse(temp.begin(),temp.end());
-            ans+=temp;
-            if(i!=-1) ans+=' ';
+            end=i;
+            reverse(s.begin()+start,s.begin()+end);
+            if(j<n)
+            {
+                s[i]=' ';
+                i++;
+            }
         }
-        return ans;
+        if(i>0 && s[i-1]==' ') i--;
+        return s.substr(0,i);
+
+        // while(s.back()==' ') s.pop_back();
+        // reverse(s.begin(),s.end());
+        // while(s.back()==' ') s.pop_back();
+        // reverse(s.begin(),s.end());
+
+        // int n=s.length();
+        // string ans="";
+        // int i=n-1;
+        // while(i>=0)
+        // {
+        //     string temp="";
+        //     while(i>=0 && s[i]!=' ')
+        //     {
+        //         temp+=s[i];
+        //         i--;
+        //     }
+        //     while(i>=0 && s[i]==' ') i--;
+        //     reverse(temp.begin(),temp.end());
+        //     ans+=temp;
+        //     if(i!=-1) ans+=' ';
+        // }
+        // return ans;
         
     }
 };
