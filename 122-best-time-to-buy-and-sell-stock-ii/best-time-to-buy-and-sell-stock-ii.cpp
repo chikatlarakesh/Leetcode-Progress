fbe6ee9e679f46d0dcfefd04af3587ef9f67336f
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-27 17:05:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (70.14%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -26,33 +26,64 @@ public:
     //     return totalProfit(0,n,prices,hasStock,dp);
     // }
 
-    int maxProfit(vector<int>& prices) {
-        int hasStock=1;
+    // int maxProfit(vector<int>& prices) {
+    //     int hasStock=1;
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n+1,vector<int>(2,0));
+
+    //     for(int i=n;i>=0;i--)
+    //     {
+    //         for(int hasStock=0;hasStock<2;hasStock++)
+    //         {
+    //             if(i==n) dp[i][hasStock]=0;
+    //             else
+    //             {
+    //                 int buy=0,notBuy=0,sell=0,notSell=0;
+    //                 if(hasStock)
+    //                 {
+    //                     buy= -prices[i]+dp[i+1][0];
+    //                     notBuy= dp[i+1][1];
+    //                 }
+    //                 else
+    //                 {
+    //                     sell= prices[i]+dp[i+1][1];
+    //                     notSell= dp[i+1][0];
+    //                 }
+    //                 dp[i][hasStock]=max({buy,notBuy,sell,notSell});
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1];
+    // }
+
+     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        int hasStock=1;
+        vector<int> next(2,0),curr(2,0);
 
         for(int i=n;i>=0;i--)
         {
             for(int hasStock=0;hasStock<2;hasStock++)
             {
-                if(i==n) dp[i][hasStock]=0;
+                if(i==n) curr[hasStock]=0;
                 else
                 {
                     int buy=0,notBuy=0,sell=0,notSell=0;
                     if(hasStock)
                     {
-                        buy= -prices[i]+dp[i+1][0];
-                        notBuy= dp[i+1][1];
+                        buy= -prices[i]+next[0];
+                        notBuy= next[1];
                     }
                     else
                     {
-                        sell= prices[i]+dp[i+1][1];
-                        notSell= dp[i+1][0];
+                        sell= prices[i]+next[1];
+                        notSell= next[0];
                     }
-                    dp[i][hasStock]=max({buy,notBuy,sell,notSell});
+                    curr[hasStock]=max({buy,notBuy,sell,notSell});
                 }
             }
+            next=curr;
         }
-        return dp[0][1];
+        return curr[1];
     }
 };
