130ec5f633abfc816567232bf8e40bcd0f0483bc
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-27 22:32:31 +0530

    Time: 3 ms (35.44%) | Memory: 16.1 MB (64.22%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
     // MEMOIZATION
-    int totalProfit(int i,int n,vector<int>& prices,int hasStock,vector<vector<int>>& dp)
-    {
-        if(i>=n) return 0;
-        if(dp[i][hasStock]!=-1) return dp[i][hasStock];
+    // int totalProfit(int i,int n,vector<int>& prices,int hasStock,vector<vector<int>>& dp)
+    // {
+    //     if(i>=n) return 0;
+    //     if(dp[i][hasStock]!=-1) return dp[i][hasStock];
 
-        int buy=0,notBuy=0,sell=0,notSell=0;
-        if(hasStock)
-        {
-            buy= -prices[i]+totalProfit(i+1,n,prices,0,dp);
-            notBuy= totalProfit(i+1,n,prices,1,dp);
-        }
-        else
-        {
-            sell= prices[i]+totalProfit(i+2,n,prices,1,dp);
-            notSell= totalProfit(i+1,n,prices,0,dp);
-        }
-        return dp[i][hasStock]=max({buy,notBuy,sell,notSell});
-    }
+    //     int buy=0,notBuy=0,sell=0,notSell=0;
+    //     if(hasStock)
+    //     {
+    //         buy= -prices[i]+totalProfit(i+1,n,prices,0,dp);
+    //         notBuy= totalProfit(i+1,n,prices,1,dp);
+    //     }
+    //     else
+    //     {
+    //         sell= prices[i]+totalProfit(i+2,n,prices,1,dp);
+    //         notSell= totalProfit(i+1,n,prices,0,dp);
+    //     }
+    //     return dp[i][hasStock]=max({buy,notBuy,sell,notSell});
+    // }
 
+
+    // int maxProfit(vector<int>& prices) {
+    //     int hasStock=1;
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n,vector<int>(2,-1));
+    //     return totalProfit(0,n,prices,hasStock,dp);
+    // }
 
     int maxProfit(vector<int>& prices) {
         int hasStock=1;
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return totalProfit(0,n,prices,hasStock,dp);
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int hasStock=0;hasStock<2;hasStock++)
+            {
+                int buy=0,notBuy=0,sell=0,notSell=0;
+                if(hasStock)
+                {
+                    buy= -prices[i]+dp[i+1][0];
+                    notBuy= dp[i+1][1];
+                }
+                else
+                {
+                    sell= prices[i]+dp[i+2][1];
+                    notSell= dp[i+1][0];
+                }
+                dp[i][hasStock]=max({buy,notBuy,sell,notSell});
+            }
+        }
+        return dp[0][1];
     }
 };
