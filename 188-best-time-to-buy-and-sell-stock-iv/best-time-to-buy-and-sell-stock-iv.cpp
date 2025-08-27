793c3c8ea90fb40cbe08fa9693ed38999cd65d3e
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-27 17:58:44 +0530

    Time: 6 ms (66.49%) | Memory: 14.4 MB (83.33%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -32,47 +32,10 @@ public:
 
 
     // TABULATION
-    int maxProfit(int k,vector<int>& prices) {
-        int n=prices.size();
-        int hasStock=1; // 1 -> empty
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        
-        for(int i=n;i>=0;i--)
-        {
-            for(int hasStock=0;hasStock<2;hasStock++)
-            {
-                for(int count=0;count<k+1;count++)
-                {
-                    if(i==n || count==0) dp[i][hasStock][count]=0;
-                    else
-                    {
-                        int buy=0,notBuy=0,sell=0,notSell=0;
-                        if(hasStock)
-                        {
-                            buy= -prices[i]+dp[i+1][0][count];
-                            notBuy= dp[i+1][hasStock][count];
-                        }
-                        else
-                        {
-                            sell= prices[i]+dp[i+1][1][count-1];
-                            notSell= dp[i+1][hasStock][count];
-                        }
-                        dp[i][hasStock][count]=max({buy,notBuy,sell,notSell});
-                    }
-                }
-            }
-        }
-        return dp[0][1][k];
-    }
-
-
-
-    // SPACE OPTIMIZATION
-    // int maxProfit(int k, vector<int>& prices) {
+    // int maxProfit(int k,vector<int>& prices) {
     //     int n=prices.size();
     //     int hasStock=1; // 1 -> empty
-    //     //vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-    //     vector<vector<int>> next(2,vector<int>(k+1,-1)), curr(2,vector<int>(k+1,-1));
+    //     vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
         
     //     for(int i=n;i>=0;i--)
     //     {
@@ -80,26 +43,63 @@ public:
     //         {
     //             for(int count=0;count<k+1;count++)
     //             {
-    //                 if(i==n || count==0) curr[hasStock][count]=0;
+    //                 if(i==n || count==0) dp[i][hasStock][count]=0;
     //                 else
     //                 {
     //                     int buy=0,notBuy=0,sell=0,notSell=0;
     //                     if(hasStock)
     //                     {
-    //                         buy= -prices[i]+next[0][count];
-    //                         notBuy= next[hasStock][count];
+    //                         buy= -prices[i]+dp[i+1][0][count];
+    //                         notBuy= dp[i+1][hasStock][count];
     //                     }
     //                     else
     //                     {
-    //                         sell= prices[i]+next[1][count-1];
-    //                         notSell= next[hasStock][count];
+    //                         sell= prices[i]+dp[i+1][1][count-1];
+    //                         notSell= dp[i+1][hasStock][count];
     //                     }
-    //                     curr[hasStock][count]=max({buy,notBuy,sell,notSell});
+    //                     dp[i][hasStock][count]=max({buy,notBuy,sell,notSell});
     //                 }
     //             }
     //         }
-    //         next=curr;
     //     }
-    //     return curr[1][k];
+    //     return dp[0][1][k];
     // }
+
+
+
+    // SPACE OPTIMIZATION
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        int hasStock=1; // 1 -> empty
+        //vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        vector<vector<int>> next(2,vector<int>(k+1,-1)), curr(2,vector<int>(k+1,-1));
+        
+        for(int i=n;i>=0;i--)
+        {
+            for(int hasStock=0;hasStock<2;hasStock++)
+            {
+                for(int count=0;count<k+1;count++)
+                {
+                    if(i==n || count==0) curr[hasStock][count]=0;
+                    else
+                    {
+                        int buy=0,notBuy=0,sell=0,notSell=0;
+                        if(hasStock)
+                        {
+                            buy= -prices[i]+next[0][count];
+                            notBuy= next[hasStock][count];
+                        }
+                        else
+                        {
+                            sell= prices[i]+next[1][count-1];
+                            notSell= next[hasStock][count];
+                        }
+                        curr[hasStock][count]=max({buy,notBuy,sell,notSell});
+                    }
+                }
+            }
+            next=curr;
+        }
+        return curr[1][k];
+    }
 };
