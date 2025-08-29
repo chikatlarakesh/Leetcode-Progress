5c8c429a0d8cd0e3ecd811ac0d89a4d468c8c35b
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-29 19:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.63%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -11,16 +11,19 @@ public:
         while(low<=high)
         {
             int mid=low+(high-low)/2;
+            // single element
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            else if(nums[mid]==nums[mid-1])
+
+            // we are in the left half
+            else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
             {
-                if((mid-1)%2==1) high=mid-2;
-                else low=mid+1;
+                low=mid+1;
             }
+
+            // we are in the right half
             else
             {
-                if(mid%2==0) low=mid+2;
-                else high=mid-1;
+                high=mid-1;
             }
         }
         return -1;
