69739003e2f48abd89fce88e363ba6dac4007711
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-08-31 20:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.58%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,12 +4,33 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
 
-        int i=0,j=m-1;
-        while(i<n && j>=0)
+        // int i=0,j=m-1;
+        // while(i<n && j>=0)
+        // {
+        //     if(matrix[i][j]==target) return true;
+        //     else if(matrix[i][j]<target) i++;
+        //     else j--;
+        // }
+        // return false;
+
+        int low=0,high=n-1,col=0;
+        while(low<=high)
         {
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]<target) i++;
-            else j--;
+            int mid=low+(high-low)/2;
+            if(matrix[mid][col]==target) return true;
+            else if(matrix[mid][col]>target) high=mid-1;
+            else low=mid+1;
+        }
+        if(high<0) return false;
+
+        int row=high;
+        low=0, high=m-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(matrix[row][mid]==target) return true;
+            else if(matrix[row][mid]>target) high=mid-1;
+            else low=mid+1;
         }
         return false;
     }
