0163912f91560be5e5800fc533b43a1b4b32a4da
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-09-04 20:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (88.80%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int distance1=abs(x-z);
+        int distance2=abs(y-z);
+        if(distance1==distance2) return 0;
+        else if(distance1<distance2) return 1;
+        else return 2;
+    }
+};
