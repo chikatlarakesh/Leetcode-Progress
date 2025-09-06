f47fb404ce3decf3a6c41851eba1e45d79437014
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-09-06 11:34:49 +0530

    Time: 25 ms (83.07%) | Memory: 10.6 MB (89.25%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    bool wildcardMatching(int i,int j,string& s,string& p,vector<vector<int>>& dp)
-    {
-        if(i<0 && j<0) return true;
-        if(j<0) return false;
-        if(i<0)
-        {
-            int k=j;
-            while(k>=0)
-            {
-                if(p[k]!='*') return false;
-                k--;
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s[i]==p[j] || p[j]=='?') return dp[i][j]= wildcardMatching(i-1,j-1,s,p,dp);
-        if(p[j]=='*') return dp[i][j]= (wildcardMatching(i,j-1,s,p,dp) || wildcardMatching(i-1,j,s,p,dp));
+    // MEMOIZATION
+    // bool wildcardMatching(int i,int j,string& s,string& p,vector<vector<int>>& dp)
+    // {
+    //     if(i<0 && j<0) return true;
+    //     if(j<0) return false;
+    //     if(i<0)
+    //     {
+    //         int k=j;
+    //         while(k>=0)
+    //         {
+    //             if(p[k]!='*') return false;
+    //             k--;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        return dp[i][j]= false;
-    }
+    //     if(s[i]==p[j] || p[j]=='?') return dp[i][j]= wildcardMatching(i-1,j-1,s,p,dp);
+    //     if(p[j]=='*') return dp[i][j]= (wildcardMatching(i,j-1,s,p,dp) || wildcardMatching(i-1,j,s,p,dp));
 
-    bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return wildcardMatching(n-1,m-1,s,p,dp);
-    }
+    //     return dp[i][j]= false;
+    // }
 
+    // bool isMatch(string s, string p) {
+    //     int n=s.length();
+    //     int m=p.length();
+    //     vector<vector<int>> dp(n,vector<int>(m,-1));
+    //     return wildcardMatching(n-1,m-1,s,p,dp);
+    // }
+
+
+    // TABULATION
     // bool isMatch(string s, string p) {
     //     int n=s.length();
     //     int m=p.length();
@@ -65,4 +69,44 @@ public:
     //     }
     //     return dp[n][m];
     // }
+
+
+    // SPACE OPTIMIZATION
+    bool isMatch(string s, string p) {
+        int n=s.length();
+        int m=p.length();
+        vector<int> curr(m+1,-1), prev(m+1,-1);
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0 && j==0) curr[j]=true;
+                else if(j==0) curr[j]=false;
+                else if(i==0)
+                {
+                    bool flag=true;
+                    int k=j;
+                    while(k>=1)
+                    {
+                        if(p[k-1]!='*')
+                        {
+                            flag=false;
+                            curr[j]=false;
+                        }
+                        k--;
+                    }
+                    if(flag) curr[j]=true;
+                }
+                else
+                {
+                    if(s[i-1]==p[j-1] || p[j-1]=='?') curr[j]= prev[j-1];
+                    else if(p[j-1]=='*') curr[j]= (curr[j-1] || prev[j]);
+                    else curr[j]= false;
+                }
+            }
+            prev=curr;
+        }
+        return curr[m];
+    }
 };
