dc8e56307857becc7761a53ef45c2a45aef26c00
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-09-07 00:39:00 +0530

    Time: 71 ms (58.30%) | Memory: 14.3 MB (64.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -51,30 +51,78 @@ public:
     // }
 
      // 2-D ARRAY SPACE OPTIMIZATION
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int> prev(n+1,-1),curr(n+1,-1);
+        
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int prevInd=n;prevInd>=i;prevInd--)
+    //         {
+    //             if(i==0)
+    //             {
+    //                 if(prevInd==nums.size() || nums[i]<nums[prevInd]) curr[prevInd]= 1;
+    //                 else curr[prevInd]= 0;
+    //             }
+    //             else
+    //             {
+    //                 int take=0,notTake=0;
+    //                 if(prevInd==nums.size() || nums[i]<nums[prevInd]) take= 1+prev[i];
+    //                 notTake= prev[prevInd];
+            
+    //                 curr[prevInd]=max(take,notTake);
+    //             }
+    //         }
+    //         prev=curr;
+    //     }
+    //     return prev[n];
+    // }
+
+
+    // 1-D ARRAY SPACE OPTIMIZATION
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int> prev(n+1,-1);
+        
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int prevInd=n;prevInd>=i;prevInd--)
+    //         {
+    //             if(i==0)
+    //             {
+    //                 if(prevInd==nums.size() || nums[i]<nums[prevInd]) prev[prevInd]= 1;
+    //                 else prev[prevInd]= 0;
+    //             }
+    //             else
+    //             {
+    //                 int take=0,notTake=0;
+    //                 if(prevInd==nums.size() || nums[i]<nums[prevInd]) take= 1+prev[i];
+    //                 notTake= prev[prevInd];
+            
+    //                 prev[prevInd]=max(take,notTake);
+    //             }
+    //         }
+    //     }
+    //     return prev[n];
+    // }
+
+    // MOST OPTIMAL BUT DIFFERENT TABULATION
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> prev(n+1,-1),curr(n+1,-1);
+        vector<int> dp(n,1);
         
+        int maxi=1;
         for(int i=0;i<n;i++)
         {
-            for(int prevInd=n;prevInd>=i;prevInd--)
+            for(int prevInd=0;prevInd<i;prevInd++)
             {
-                if(i==0)
+                if(nums[prevInd]<nums[i])
                 {
-                    if(prevInd==nums.size() || nums[i]<nums[prevInd]) curr[prevInd]= 1;
-                    else curr[prevInd]= 0;
-                }
-                else
-                {
-                    int take=0,notTake=0;
-                    if(prevInd==nums.size() || nums[i]<nums[prevInd]) take= 1+prev[i];
-                    notTake= prev[prevInd];
-            
-                    curr[prevInd]=max(take,notTake);
+                    dp[i]=max(1+dp[prevInd],dp[i]);
                 }
             }
-            prev=curr;
+            maxi=max(maxi,dp[i]);
         }
-        return prev[n];
+        return maxi;
     }
 };
