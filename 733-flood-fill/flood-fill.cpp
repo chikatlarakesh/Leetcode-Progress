3845bfc72d084815b9d042bf333be5b6251a5d49
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-09-18 23:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (59.81%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    void dfs(int row,int col,int initialColour,int color,vector<vector<int>>& visited,vector<pair<int,int>>& dir) {
+        visited[row][col] = color;
+
+        for(auto [r,c]: dir) {
+            int i = row + r;
+            int j = col + c;
+
+            if(i>=0 && i<visited.size() && j>=0 && j<visited[0].size() && visited[i][j] == initialColour) {
+                dfs(i,j,initialColour,color,visited,dir);
+            }
+        }
+    }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
@@ -7,25 +20,38 @@ public:
         if(initialColour == color) return image;
         vector<vector<int>> visited = image;
         vector<pair<int,int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        visited[sr][sc] = color;
 
-        while(!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for(auto [r,c]: dir) {
-                int i = row + r;
-                int j = col + c;
-
-                if(i>=0 && i<n && j>=0 && j<m && visited[i][j] == initialColour) {
-                    visited[i][j] = color;
-                    q.push({i,j});
-                }
-            }
-        }
+        dfs(sr,sc,initialColour,color,visited,dir);
         return visited;
     }
+
+    // BFS
+    // vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    //     int n = image.size();
+    //     int m = image[0].size();
+    //     int initialColour = image[sr][sc]; 
+    //     if(initialColour == color) return image;
+    //     vector<vector<int>> visited = image;
+    //     vector<pair<int,int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
+    //     queue<pair<int,int>> q;
+    //     q.push({sr,sc});
+    //     visited[sr][sc] = color;
+
+    //     while(!q.empty()) {
+    //         int row = q.front().first;
+    //         int col = q.front().second;
+    //         q.pop();
+
+    //         for(auto [r,c]: dir) {
+    //             int i = row + r;
+    //             int j = col + c;
+
+    //             if(i>=0 && i<n && j>=0 && j<m && visited[i][j] == initialColour) {
+    //                 visited[i][j] = color;
+    //                 q.push({i,j});
+    //             }
+    //         }
+    //     }
+    //     return visited;
+    // }
 };
