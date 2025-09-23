7318533326a28eb14117c8918dac54422ab5b03d
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-09-23 14:36:22 +0530

    Time: 44 ms (58.97%) | Memory: 66.2 MB (50.12%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,36 +1,73 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>& visited,vector<int>& pathVisited,vector<int>& isSafe,vector<vector<int>>& adj) {
-        visited[node] = 1;
-        pathVisited[node] = 1;
-
-        for(auto &it: adj[node]) {
-            if(!visited[it]) {
-                if(dfs(it,visited,pathVisited,isSafe,adj) == true) return true;
-            }
-            else if(pathVisited[it]) return true;
-        }
-
-        isSafe[node] = 1;
-        pathVisited[node] = 0;
-        return false;
-    }
-
+    // BFS
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
         int n = adj.size();
-        vector<int> visited(n,0), pathVisited(n,0), isSafe(n,0);
+        vector<vector<int>> adjRev(n);
+        vector<int> inDegree(n,0);
+
         for(int i=0;i<n;i++) {
-            if(!visited[i]) {
-                dfs(i,visited,pathVisited,isSafe,adj);
+            for(auto &it: adj[i]) {
+                adjRev[it].push_back(i);
+                inDegree[i]++;
             }
         }
 
+        queue<int> q;
         vector<int> safeNodes;
         for(int i=0;i<n;i++) {
-            if(isSafe[i]) {
-                safeNodes.push_back(i);
+            if(inDegree[i] == 0) q.push(i);
+        }
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safeNodes.push_back(node);
+
+            for(auto &it: adjRev[node]) {
+                inDegree[it]--;
+                if(inDegree[it] == 0) q.push(it);
             }
         }
+
+        sort(safeNodes.begin(),safeNodes.end());
         return safeNodes;
     }
+
+
+    // DFS
+    // bool dfs(int node,vector<int>& visited,vector<int>& pathVisited,vector<int>& isSafe,vector<vector<int>>& adj) {
+    //     visited[node] = 1;
+    //     pathVisited[node] = 1;
+
+    //     for(auto &it: adj[node]) {
+    //         if(!visited[it]) {
+    //             if(dfs(it,visited,pathVisited,isSafe,adj) == true) return true;
+    //         }
+    //         else if(pathVisited[it]) return true;
+    //     }
+
+    //     isSafe[node] = 1;
+    //     pathVisited[node] = 0;
+    //     return false;
+    // }
+
+    // vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+    //     int n = adj.size();
+    //     vector<int> visited(n,0), pathVisited(n,0), isSafe(n,0);
+    //     for(int i=0;i<n;i++) {
+    //         if(!visited[i]) {
+    //             dfs(i,visited,pathVisited,isSafe,adj);
+    //         }
+    //     }
+
+    //     vector<int> safeNodes;
+    //     for(int i=0;i<n;i++) {
+    //         if(isSafe[i]) {
+    //             safeNodes.push_back(i);
+    //         }
+    //     }
+    //     return safeNodes;
+    // }
+
 };
