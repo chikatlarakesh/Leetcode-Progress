33dc7908e1a117babd6b1a2ba55a9caf169b93aa
CHIKATLA RAKESH <rakeshchikatla12@gmail.com>
2025-09-24 18:46:36 +0530

    Time: 50 ms (66.83%) | Memory: 24.6 MB (41.45%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -3,17 +3,17 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        using node = pair<pair<int,int>,int>;
+        using node = pair<int,pair<int,int>>;
         priority_queue<node,vector<node>,greater<node>> pq;
         vector<vector<int>> effort(n,vector<int>(m,INT_MAX));
 
-        pq.push({{0,0},0});
+        pq.push({0,{0,0}});
         effort[0][0] = 0;
         vector<pair<int,int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         while(!pq.empty()) {
-            int row = pq.top().first.first;
-            int col = pq.top().first.second;
-            int currEffort = pq.top().second;
+            int currEffort = pq.top().first;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
             pq.pop();
             if(row == n-1 && col == m-1) return currEffort;
             if(currEffort > effort[row][col]) continue;
@@ -26,7 +26,7 @@ public:
                     int e = max(currEffort,abs(heights[row][col] - heights[i][j]));
                     if(e < effort[i][j]) {
                         effort[i][j] = e;
-                        pq.push({{i,j},e});
+                        pq.push({e,{i,j}});
                     }
                 }
             }
