8eb2c98f90d834b79e6c2956bd023edf94499846
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-09-26 23:13:43 +0530

    Time: 1 ms (83.58%) | Memory: 13.8 MB (58.95%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
+    void dfs(int node,int dst,vector<int>& result,vector<vector<int>>& paths,vector<vector<int>>& adj) {
+        result.push_back(node);
+        if(node == dst) paths.push_back(result);
+
+        for(auto &it: adj[node]) {
+            dfs(it,dst,result,paths,adj);
+        }
+        result.pop_back();
+    }
+
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj) {
         int n = adj.size();
-        queue<vector<int>> q;
-        vector<vector<int>> result;
-        q.push({0});
+        vector<vector<int>> paths;
+        vector<int> result;
+        dfs(0,n-1,result,paths,adj);
+        return paths;
 
-        while(!q.empty()) {
-            vector<int> path = q.front();
-            int node = path.back();
-            q.pop();
-            if(node == n-1) result.push_back(path);
+        // BFS
+        // int n = adj.size();
+        // queue<vector<int>> q;
+        // vector<vector<int>> result;
+        // q.push({0});
 
-            for(auto &it: adj[node]) {
-                vector<int> newPath = path;
-                newPath.push_back(it);
-                q.push(newPath);
-            }
-        }
-        return result;
+        // while(!q.empty()) {
+        //     vector<int> path = q.front();
+        //     int node = path.back();
+        //     q.pop();
+        //     if(node == n-1) result.push_back(path);
+
+        //     for(auto &it: adj[node]) {
+        //         vector<int> newPath = path;
+        //         newPath.push_back(it);
+        //         q.push(newPath);
+        //     }
+        // }
+        // return result;
     }
 };
