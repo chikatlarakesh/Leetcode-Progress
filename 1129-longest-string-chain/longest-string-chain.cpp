70e875018593585387efcec0fc993ecf20681459
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-01 16:28:23 +0530

    Time: 67 ms (47.70%) | Memory: 31.7 MB (38.20%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    class comparer {
+        public:
+        bool operator()(string& a,string& b) {
+            if(a.length() != b.length()) {
+                return a.length() < b.length();
+            }
+            return a < b;
+        }
+    };
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),comparer());
+        unordered_map<string,int> mp;
+        int n = words.size();
+        int maxLength = 0;
+
+        for(int i=0;i<n;i++) {
+            int m = words[i].length();
+            int length = 1;
+            for(int j=0;j<m;j++) {
+                string s = words[i];
+                s.erase(j,1);
+                if(mp.find(s) != mp.end()) {
+                    int currLength = mp[s];
+                    if(currLength + 1 > length) {
+                        length = currLength + 1;
+                    }
+                }
+            }
+            maxLength = max(maxLength,length);
+            mp[words[i]] = length;
+        }
+        return maxLength;
+    }
+};
