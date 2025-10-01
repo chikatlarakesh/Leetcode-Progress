79ee00218b9c485cc7a62334746ebe9bb1043938
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-01 16:55:09 +0530

    Time: 40 ms (74.29%) | Memory: 18.7 MB (84.88%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // MY APPROACH
     class comparer {
         public:
         bool operator()(const string& a,const string& b) const {
@@ -10,27 +11,62 @@ public:
         }
     };
 
+    // int longestStrChain(vector<string>& words) {
+    //     sort(words.begin(),words.end(),comparer());
+    //     unordered_map<string,int> mp;
+    //     int n = words.size();
+    //     int maxLength = 0;
+
+    //     for(int i=0;i<n;i++) {
+    //         int m = words[i].length();
+    //         int length = 1;
+    //         for(int j=0;j<m;j++) {
+    //             string s = words[i];
+    //             s.erase(j,1);
+    //             if(mp.find(s) != mp.end()) {
+    //                 int currLength = mp[s];
+    //                 if(currLength + 1 > length) {
+    //                     length = currLength + 1;
+    //                 }
+    //             }
+    //         }
+    //         maxLength = max(maxLength,length);
+    //         mp[words[i]] = length;
+    //     }
+    //     return maxLength;
+    // }
+
+    // STRIVER APPROACH
+
+    bool isPossible(string& s1,string& s2) {
+        if(s1.length() != s2.length() + 1) return false;
+
+        int i = 0, j = 0;
+        while(i < s1.length()) {
+            if(s1[i] == s2[j]) {
+                i++;
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return (i == s1.length() && j == s2.length());
+    }
+
     int longestStrChain(vector<string>& words) {
         sort(words.begin(),words.end(),comparer());
-        unordered_map<string,int> mp;
         int n = words.size();
+        vector<int> dp(n,1);
         int maxLength = 0;
 
         for(int i=0;i<n;i++) {
-            int m = words[i].length();
-            int length = 1;
-            for(int j=0;j<m;j++) {
-                string s = words[i];
-                s.erase(j,1);
-                if(mp.find(s) != mp.end()) {
-                    int currLength = mp[s];
-                    if(currLength + 1 > length) {
-                        length = currLength + 1;
-                    }
+            for(int prevInd = 0;prevInd<i;prevInd++) {
+                if(isPossible(words[i],words[prevInd]) && dp[prevInd] + 1 > dp[i]) {
+                    dp[i] = dp[prevInd] + 1;
                 }
             }
-            maxLength = max(maxLength,length);
-            mp[words[i]] = length;
+            maxLength = max(maxLength,dp[i]);
         }
         return maxLength;
     }
