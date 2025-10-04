7f0f3be69503c6e94922d6c2bf22000023cad12f
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-04 12:29:10 +0530

    Time: 38 ms (35.32%) | Memory: 13.7 MB (89.38%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int mcm(int i,int j,vector<int>& nums,vector<vector<int>>& dp) {
+    int mcm(int i,int j,int start,int end,vector<int>& nums,vector<vector<int>>& dp) {
         if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
         int minCost = INT_MAX;
         for(int k=i;k<=j;k++) {
-            int cost = (nums[j+1] - nums[i-1]) + mcm(i,k-1,nums,dp) + mcm(k+1,j,nums,dp);
+            int leftCost = mcm(i,k-1,start,nums[k],nums,dp);
+            int rightCost = mcm(k+1,j,nums[k],end,nums,dp);
+            int cost = (end - start) + leftCost + rightCost;
             minCost = min(minCost,cost);
         }
         return dp[i][j] = minCost;
     }
 
     int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(n);
-        cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
         int m = cuts.size();
         vector<vector<int>> dp(m,vector<int>(m,-1));
-        return mcm(1,m-2,cuts,dp);
+        return mcm(0,m-1,0,n,cuts,dp);
     }
 };
