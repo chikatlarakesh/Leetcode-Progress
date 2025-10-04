3409bdf44759be26b1311bdada4cd20ca7e3ac28
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-04 19:50:22 +0530

    Time: 210 ms (17.17%) | Memory: 13.8 MB (59.52%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
-    // int maximumCoins(int i,int j,vector<int>& nums,vector<vector<int>> &dp) {
-    //     if(i > j) return 0;
-    //     if(dp[i][j] != -1) return dp[i][j];
+    // MEMOIZATION
+    int maximumCoins(int i,int j,vector<int>& nums,vector<vector<int>> &dp) {
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     int maxCoins = INT_MIN;
-    //     for(int k=i;k<=j;k++) {
-    //         int left = maximumCoins(i,k-1,nums,dp);
-    //         int right = maximumCoins(k+1,j,nums,dp);
-    //         int coins = (nums[i - 1] * nums[k] * nums[j + 1]) + left + right;
-    //         maxCoins = max(maxCoins,coins);
-    //     }
-    //     return dp[i][j] = maxCoins;
-    // }
-
-    // int maxCoins(vector<int>& nums) {
-    //     nums.insert(nums.begin(),1);
-    //     nums.push_back(1);
-    //     int n = nums.size();
-    //     vector<vector<int>> dp(n,vector<int>(n,-1));
-    //     return maximumCoins(1,n-2,nums,dp);
-    // }
+        int maxCoins = 0;
+        for(int k=i;k<=j;k++) {
+            int left = maximumCoins(i,k-1,nums,dp);
+            int right = maximumCoins(k+1,j,nums,dp);
+            int coins = (nums[i - 1] * nums[k] * nums[j + 1]) + left + right;
+            maxCoins = max(maxCoins,coins);
+        }
+        return dp[i][j] = maxCoins;
+    }
 
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        
-        for(int i=n-2;i>=1;i--) {
-            for(int j=1;j<=n-2;j++) {
-                int maxCoins = 0;
-                for(int k=i;k<=j;k++) {
-                    int left = dp[i][k-1];
-                    int right = dp[k+1][j];
-                    int coins = (nums[i - 1] * nums[k] * nums[j + 1]) + left + right;
-                    maxCoins = max(maxCoins,coins);
-                }
-                dp[i][j] = maxCoins;
-            }
-        }
-        return dp[1][n-2];
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return maximumCoins(1,n-2,nums,dp);
     }
+
+
+    // TABULATION
+    // int maxCoins(vector<int>& nums) {
+    //     nums.insert(nums.begin(),1);
+    //     nums.push_back(1);
+    //     int n = nums.size();
+    //     vector<vector<int>> dp(n,vector<int>(n,0));
+        
+    //     for(int i=n-2;i>=1;i--) {
+    //         for(int j=1;j<=n-2;j++) {
+    //             int maxCoins = 0;
+    //             for(int k=i;k<=j;k++) {
+    //                 int left = dp[i][k-1];
+    //                 int right = dp[k+1][j];
+    //                 int coins = (nums[i - 1] * nums[k] * nums[j + 1]) + left + right;
+    //                 maxCoins = max(maxCoins,coins);
+    //             }
+    //             dp[i][j] = maxCoins;
+    //         }
+    //     }
+    //     return dp[1][n-2];
+    //}
 };
