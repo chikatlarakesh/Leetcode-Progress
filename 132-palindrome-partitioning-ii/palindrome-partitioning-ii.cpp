69c1bff7f1e3c4f345c64a639853cdd1ac538185
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-05 18:54:41 +0530

    Time: 1500 ms (41.90%) | Memory: 9.3 MB (84.75%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -9,23 +9,40 @@ public:
         return true;
     }
 
-    int minimumCuts(int i,int n,string& s,vector<int>& dp) {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
+    // int minimumCuts(int i,int n,string& s,vector<int>& dp) {
+    //     if(i == n) return 0;
+    //     if(dp[i] != -1) return dp[i];
 
-        int minCuts = INT_MAX;
-        for(int j=i;j<n;j++) {
-            if(isPalindrome(i,j,s)) {
-                int cuts = 1 + minimumCuts(j+1,n,s,dp);
-                minCuts = min(minCuts,cuts);
-            }
-        }
-        return dp[i] = minCuts;
-    }
+    //     int minCuts = INT_MAX;
+    //     for(int j=i;j<n;j++) {
+    //         if(isPalindrome(i,j,s)) {
+    //             int cuts = 1 + minimumCuts(j+1,n,s,dp);
+    //             minCuts = min(minCuts,cuts);
+    //         }
+    //     }
+    //     return dp[i] = minCuts;
+    // }
+
+    // int minCut(string s) {
+    //     int n = s.length();
+    //     vector<int> dp(n,-1);
+    //     return minimumCuts(0,n,s,dp) - 1;
+    // }
 
     int minCut(string s) {
         int n = s.length();
-        vector<int> dp(n,-1);
-        return minimumCuts(0,n,s,dp) - 1;
+        vector<int> dp(n+1,0);
+        
+        for(int i=n-1;i>=0;i--) {
+            int minCuts = INT_MAX;
+            for(int j=i;j<n;j++) {
+                if(isPalindrome(i,j,s)) {
+                    int cuts = 1 + dp[j+1];
+                    minCuts = min(minCuts,cuts);
+                }
+            }
+            dp[i] = minCuts;
+        }
+        return dp[0] - 1;
     }
 };
