4e89875bb670e8a463dd36019549373156425c6f
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-10 22:46:31 +0530

    Time: 250 ms (85.13%) | Memory: 46 MB (28.21%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     long long minTime(vector<int>& skill, vector<int>& mana) {
-        int n = skill.size(), m = mana.size();
-        vector<long long> done(n + 1, 0);
-        
-        for (int j = 0; j < m; ++j) {
-            for (int i = 0; i < n; ++i) {
-                done[i + 1] = max(done[i + 1], done[i]) + 1LL * mana[j] * skill[i];
+        using ll = long long;
+        int m = skill.size();
+        int n = mana.size();
+        vector<ll> currTime(m), finishTime(n);
+        ll prevTime = 0;
+
+        // 1st potion brew
+        for(int j=0;j<m;j++) {
+            currTime[j] = (skill[j] * mana[0]) + prevTime;
+            prevTime = currTime[j];
+        }
+        finishTime = currTime;
+
+        // brew all the remaining potions
+        for(int i=1;i<n;i++) {
+            currTime[0] = finishTime[0] + (mana[i] * skill[0]);
+            for(int j=1;j<m;j++) {
+                currTime[j] = max(currTime[j-1],finishTime[j]) + (mana[i] * skill[j]);
             }
-            for (int i = n - 1; i > 0; --i) {
-                done[i] = done[i + 1] - 1LL * mana[j] * skill[i];
+
+            finishTime[m-1] = currTime[m-1];
+            for(int k=m-1;k>0;k--) {
+                finishTime[k-1] = finishTime[k] - (mana[i] * skill[k]);
             }
         }
-        
-        return done[n];
+        return (long long) finishTime[m-1];
     }
 };
