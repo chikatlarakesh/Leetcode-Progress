2b4029920f3a5af36b6bc0315ec081535791519a
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-10 23:25:23 +0530

    Time: 128 ms (88.03%) | Memory: 152.7 MB (17.25%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int maxEnergy(int i,int n,int k,vector<int>& energy,vector<int>& dp) {
-        if(i>=n) return 0;
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = energy[i] + maxEnergy(i+k,n,k,energy,dp);
-    }
+    // MEMOIZATION
+    // int maxEnergy(int i,int n,int k,vector<int>& energy,vector<int>& dp) {
+    //     if(i>=n) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     return dp[i] = energy[i] + maxEnergy(i+k,n,k,energy,dp);
+    // }
+
+    // int maximumEnergy(vector<int>& energy, int k) {
+    //     int n = energy.size();
+    //     int maxi = INT_MIN;
+    //     vector<int> dp(n,-1);
+    //     for(int i=0;i<n;i++) {
+    //         maxi = max(maxi,maxEnergy(i,n,k,energy,dp));
+    //     }
+    //     return maxi;
+    // }
 
     int maximumEnergy(vector<int>& energy, int k) {
         int n = energy.size();
+        vector<int> dp(n+k,0);
         int maxi = INT_MIN;
-        vector<int> dp(n,-1);
-        for(int i=0;i<n;i++) {
-            maxi = max(maxi,maxEnergy(i,n,k,energy,dp));
+        for(int i=n-1;i>=0;i--) {
+            dp[i] = energy[i] + dp[i+k];
+            maxi = max(maxi,dp[i]);
         }
         return maxi;
     }
