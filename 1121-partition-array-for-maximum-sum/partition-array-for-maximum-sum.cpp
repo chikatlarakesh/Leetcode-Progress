db0741f9cb9d9f6288199ce145142c8e37567194
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-13 18:17:10 +0530

    Time: 2 ms (89.40%) | Memory: 12.5 MB (61.30%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    int maxSum(int i,int k,vector<int>& arr,vector<int>& dp) {
-        int n = arr.size();
-        if(i >= n) return 0;
-        if(dp[i]!= -1) return dp[i];
+    // int maxSum(int i,int k,vector<int>& arr,vector<int>& dp) {
+    //     int n = arr.size();
+    //     if(i >= n) return 0;
+    //     if(dp[i]!= -1) return dp[i];
 
-        int maxi = 0, maxValue = 0;
-        for(int j=i;j<n && j<i+k;j++) {
-            maxValue = max(maxValue,arr[j]);
-            int length = j - i + 1;
-            int sum = (maxValue * length) + maxSum(j+1,k,arr,dp);
-            maxi = max(sum,maxi);
-        }
-        return dp[i] = maxi;
-    }
+    //     int maxi = 0, maxValue = 0;
+    //     for(int j=i;j<n && j<i+k;j++) {
+    //         maxValue = max(maxValue,arr[j]);
+    //         int length = j - i + 1;
+    //         int sum = (maxValue * length) + maxSum(j+1,k,arr,dp);
+    //         maxi = max(sum,maxi);
+    //     }
+    //     return dp[i] = maxi;
+    // }
+
+    // int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    //     int n = arr.size();
+    //     vector<int> dp(n,-1);
+    //     return maxSum(0,k,arr,dp);
+    // }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n,-1);
-        return maxSum(0,k,arr,dp);
+        vector<int> dp(n+1,0);
+        
+        for(int i=n-1;i>=0;i--) {
+            int maxi = 0, maxValue = 0;
+            for(int j=i;j<n && j<i+k;j++) {
+                maxValue = max(maxValue,arr[j]);
+                int length = j - i + 1;
+                int sum = (maxValue * length) + dp[j+1];
+                maxi = max(sum,maxi);
+            }
+            dp[i] = maxi;
+        }
+        return dp[0];
     }
 };
