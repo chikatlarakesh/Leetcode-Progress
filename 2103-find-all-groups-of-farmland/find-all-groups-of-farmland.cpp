a7a838c008357df484476906e44faccd878e4646
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-19 16:54:35 +0530

    Time: 151 ms (11.14%) | Memory: 156.3 MB (7.73%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> bfs(int row, int col, vector<vector<int>>& visited,vector<vector<int>>& land) {
+        int n = land.size();
+        int m = land[0].size();
+        visited[row][col] = 1;
+
+        queue<pair<int,int>> q;
+        vector<int> result;
+        result.push_back(row);
+        result.push_back(col);
+        q.push({row,col});
+
+        vector<pair<int,int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while(!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(auto &[x,y]: dir) {
+                int i = r + x;
+                int j = c + y;
+
+                if(i>=0 && i<n && j>=0 && j<m && !visited[i][j] && land[i][j] == 1) {
+                    visited[i][j] = 1;
+                    q.push({i,j});
+                }
+            }
+
+            if(q.empty()) {
+                result.push_back(r);
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        int n = land.size();
+        int m = land[0].size();
+
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        vector<vector<int>> result;
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(!visited[i][j] && land[i][j] == 1) {
+                    result.push_back(bfs(i,j,visited,land));
+                }
+            }
+        }
+        return result;
+    }
+};
