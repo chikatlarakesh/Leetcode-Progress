6164e9c889db7018efb5d47a289117259fceaa4a
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-20 14:04:01 +0530

    Time: 233 ms (48.44%) | Memory: 64.1 MB (69.53%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,41 +1,78 @@
 class Solution {
 public:
+    // string findLexSmallestString(string s, int a, int b) {
+    //     int n = s.length();
+    //     unordered_set<string> visited;
+    //     queue<string> q;
+    //     string ans = s;
+    //     q.push(s);
+    //     visited.insert(s);
+
+    //     while(!q.empty()) {
+    //         string temp = q.front();
+    //         q.pop();
+    //         if(temp < ans) ans = temp;
+
+    //         // add
+    //         string addedString = temp;
+    //         for(int i=1;i<n;i+=2) {
+    //             int val = (addedString[i] - '0') + a;
+    //             val = val % 10;
+    //             addedString[i] = val + '0';
+    //         }
+    //         if(visited.find(addedString) == visited.end()) {
+    //             visited.insert(addedString);
+    //             q.push(addedString);
+    //         }
+
+    //         // rotate
+    //         string rotatedString = temp;
+    //         for(int i=0;i<n;i++) {
+    //             int ind = (i + b) % n;
+    //             rotatedString[ind] = temp[i];
+    //         }
+    //         if(visited.find(rotatedString) == visited.end()) {
+    //             visited.insert(rotatedString);
+    //             q.push(rotatedString);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    string ans;
+    void dfs(int n,string& s,int a,int b, unordered_set<string>& visited) {
+        if(visited.count(s)) return;
+        visited.insert(s);
+        if(s < ans) ans = s;
+
+        // add
+        string addedString = s;
+        for(int i=1;i<n;i+=2) {
+            int val = (addedString[i] - '0') + a;
+            val = val % 10;
+            addedString[i] = val + '0';
+        }
+        if(!visited.count(addedString)) {
+            dfs(n,addedString,a,b,visited);
+        }
+
+        // rotate
+        string rotatedString = s;
+        for(int i=0;i<n;i++) {
+            int ind = (i + b) % n;
+            rotatedString[ind] = s[i];
+        }
+        if(!visited.count(rotatedString)) {
+            dfs(n,rotatedString,a,b,visited);
+        }      
+    }
+
     string findLexSmallestString(string s, int a, int b) {
         int n = s.length();
         unordered_set<string> visited;
-        queue<string> q;
-        string ans = s;
-        q.push(s);
-        visited.insert(s);
+        ans = s;
 
-        while(!q.empty()) {
-            string temp = q.front();
-            q.pop();
-            if(temp < ans) ans = temp;
-
-            // add
-            string addedString = temp;
-            for(int i=1;i<n;i+=2) {
-                int val = (addedString[i] - '0') + a;
-                val = val % 10;
-                addedString[i] = val + '0';
-            }
-            if(visited.find(addedString) == visited.end()) {
-                visited.insert(addedString);
-                q.push(addedString);
-            }
-
-            // rotate
-            string rotatedString = temp;
-            for(int i=0;i<n;i++) {
-                int ind = (i + b) % n;
-                rotatedString[ind] = temp[i];
-            }
-            if(visited.find(rotatedString) == visited.end()) {
-                visited.insert(rotatedString);
-                q.push(rotatedString);
-            }
-        }
+        dfs(n,s,a,b,visited);
         return ans;
     }
 };
