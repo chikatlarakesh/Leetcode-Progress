a5d78326b6c68daf99d4c3c15b3e2760d3fde288
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-20 11:22:49 +0530

    Time: 119 ms (59.50%) | Memory: 97.8 MB (34.55%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int cnt = 0, prev = INT_MIN;
-        for (int num : nums) {
-            int curr = min(max(num - k, prev + 1), num + k);
-            if (curr > prev) {
-                cnt++;
-                prev = curr;
+        int n = nums.size();
+        int count = 1;
+        sort(nums.begin(),nums.end());
+        nums[0] = nums[0] - k;
+
+        for(int i=1;i<n;i++) {
+            int bound = nums[i] + k;
+            if(nums[i-1] < nums[i] - k) {
+                nums[i] = nums[i] - k;
+                count++;
+            }
+            else {
+                int bound = nums[i] + k;
+                if(nums[i-1] + 1 <= bound) {
+                    nums[i] = nums[i-1] + 1;
+                    count++;
+                }
+                else nums[i] = nums[i-1];
             }
         }
-        return cnt;
+        return count;
     }
 };
