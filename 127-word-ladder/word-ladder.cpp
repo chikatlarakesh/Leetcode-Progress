c6f463f950d2bde162e45a17213b1329439219fe
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-20 17:00:05 +0530

    Time: 53 ms (75.30%) | Memory: 21.2 MB (63.59%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    using Node = pair<int,string>;
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         int n = beginWord.length();
-        unordered_set<string> visited;
-        for(string word: wordList) visited.insert(word);
-        if(!visited.count(endWord)) return 0;
+        unordered_set<string> s(wordList.begin(),wordList.end());
+        if(s.find(endWord) == s.end()) return 0;
 
-        // {steps,word};
-        priority_queue<Node,vector<Node>,greater<Node>> pq;
-        pq.push({1,beginWord});
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        s.erase(beginWord);
 
-        while(!pq.empty()) {
-            int steps = pq.top().first;
-            string word = pq.top().second;
-            pq.pop();
-            visited.erase(word);
+        while(!q.empty()) {
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
             if(word == endWord) return steps;
 
             for(int i=0;i<n;i++) {
-                string temp = word;
-                for(char ch = 'a';ch <= 'z'; ch++) {
-                    temp[i] = ch;
-                    if(visited.count(temp)) {
-                        pq.push({steps+1,temp});
+                char originalChar = word[i];
+                for(char ch='a';ch<='z';ch++) {
+                    word[i] = ch;
+                    if(s.find(word) != s.end()) {
+                        s.erase(word);
+                        q.push({word,steps+1});
                     }
                 }
+                word[i] = originalChar;
             }
         }
         return 0;
