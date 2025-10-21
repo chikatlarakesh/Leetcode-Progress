09d15fda57484566adde9734b51c5ce3a7bbc0a1
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-21 10:20:27 +0530

    Time: 27 ms (28.60%) | Memory: 17.1 MB (18.17%) - LeetSync

---
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    using Node = pair<int,pair<pair<int,int>,vector<vector<int>>>>;
+    int slidingPuzzle(vector<vector<int>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> solvedBoard = {{1,2,3},{4,5,0}};
+        set<vector<vector<int>>> visited;
+
+        // find zero coordinates
+        int row = 0,col = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+
+        queue<Node> q;
+        q.push({0,{{row,col},board}});
+        visited.insert(board);
+
+        vector<pair<int,int>> dir = {{-1, 0},{0, 1},{1, 0},{0, -1}};
+        while(!q.empty()) {
+            auto it = q.front();
+            int moves = it.first;
+            int row = it.second.first.first;
+            int col = it.second.first.second;
+            vector<vector<int>> currBoard = it.second.second;
+            if(currBoard == solvedBoard) return moves;
+            q.pop();
+
+            for(auto &[r,c]: dir) {
+                int i = row + r;
+                int j = col + c;
+                
+                if(i>=0 && i<n && j>=0 && j<m) {
+                    swap(currBoard[row][col],currBoard[i][j]);
+                    if(!visited.count(currBoard)) {
+                        visited.insert(currBoard);
+                        q.push({moves+1,{{i,j},currBoard}});
+                    }
+                    swap(currBoard[row][col],currBoard[i][j]);
+                }
+            }
+        }
+        return -1;
+    }
+};
