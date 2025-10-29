633159c596498f27bd9002eb6f3a1ff9ca357be7
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-29 11:30:45 +0530

    Time: 1 ms (27.46%) | Memory: 16.9 MB (6.85%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,76 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* constructBST(vector<int>& preorder,int& i,int bound)
-    {
-        if(i == preorder.size() || preorder[i] > bound) return NULL;
+    TreeNode* constructBST(int preStart,int preEnd,vector<int>& preorder,int inStart,int inEnd, vector<int>& inorder,unordered_map<int,int>& inorderMap) {
 
-        TreeNode* root = new TreeNode(preorder[i++]);
-        root->left = constructBST(preorder,i,root->val);
-        root->right = constructBST(preorder,i,bound);
+        if(preStart > preEnd || inStart > inEnd) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int index = inorderMap[root->val];
+        int left = index - inStart;
+
+        root->left = constructBST(preStart+1,preStart+left,preorder,inStart,index-1,inorder,inorderMap);
+
+        root->right = constructBST(preStart+left+1,preEnd,preorder,index+1,inEnd,inorder,inorderMap);
+
         return root;
     }
 
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int i = 0;
-        return constructBST(preorder,i,INT_MAX);
+        int n = preorder.size();
+        vector<int> inorder = preorder;
+        sort(inorder.begin(),inorder.end());
+        unordered_map<int,int> inorderMap;
+        for(int i=0;i<n;i++) {
+            inorderMap[inorder[i]] = i;
+        }
+
+        return constructBST(0,n-1,preorder,0,n-1,inorder,inorderMap);
+
     }
-
-
-
-
-
-
-    // TreeNode* constructBST(vector<int>& preorder,int start,int end) {
-    //     if(start > end) return NULL;
-
-    //     int lowerbound = lower_bound(preorder.begin()+start+1,preorder.begin()+end+1,preorder[start]) - preorder.begin();
-    //     TreeNode* root = new TreeNode(preorder[start]);
-
-    //     root->left = constructBST(preorder,start+1,lowerbound-1);
-    //     root->right = constructBST(preorder,lowerbound,end);
-    //     return root;
-    // }
-
-    // TreeNode* bstFromPreorder(vector<int>& preorder) {
-    //     int n = preorder.size();
-    //     return constructBST(preorder,0,n-1);
-    // }
-
-
-
-
-
-
-
-
-    // TreeNode* constructBST(vector<int>& preorder,int preStart,int preEnd,vector<int>& inorder,int inStart,int inEnd,unordered_map<int,int>& inorderMap) {
-    //     if(preStart > preEnd || inStart > inEnd) return NULL;
-
-    //     TreeNode* root = new TreeNode(preorder[preStart]);
-    //     int inRoot = inorderMap[root->val];
-    //     int numsLeft = inRoot - inStart;
-
-    //     root->left = constructBST(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,inorderMap);
-
-    //     root->right = constructBST(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,inorderMap);
-
-    //     return root;
-    // }
-
-    // TreeNode* bstFromPreorder(vector<int>& preorder) {
-    //     int n = preorder.size();
-    //     vector<int> inorder(preorder.begin(),preorder.end());
-    //     sort(inorder.begin(),inorder.end());
-    //     unordered_map<int,int> inorderMap;
-    //     for(int i=0;i<n;i++) inorderMap[inorder[i]] = i;
-
-    //     return constructBST(preorder,0,n-1,inorder,0,n-1,inorderMap);
-    // }
 };
