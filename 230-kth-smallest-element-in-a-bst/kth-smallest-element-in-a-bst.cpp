0824cba1916d4e77fefcb61be1b4598707b35768
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-10-31 21:59:07 +0530

    Time: 2 ms (10.02%) | Memory: 24.2 MB (90.16%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -11,38 +11,18 @@
  */
 class Solution {
 public:
-    void findKthSmallest(TreeNode* root,int k,int& count,int& ans)
-    {
-        if(root==NULL || ans!=-1) return;
+    void findKthSmallest(TreeNode* root,int& ans,int& k) {
+        if(root == NULL) return;
 
-        findKthSmallest(root->left,k,count,ans);
-        count++;
-        if(count==k)
-        {
-            ans = root->val; 
-            return;
-        }
-        findKthSmallest(root->right,k,count,ans);
+        findKthSmallest(root->left,ans,k);
+        k--;
+        if(k == 0) ans = root->val;
+        findKthSmallest(root->right,ans,k);
     }
 
     int kthSmallest(TreeNode* root, int k) {
-        int count=0,ans=-1;
-        findKthSmallest(root,k,count,ans);
+        int ans = -1;
+        findKthSmallest(root,ans,k);
         return ans;
-
-        // queue<TreeNode*> q;
-        // priority_queue<int> pq;
-        // q.push(root);
-        // while(!q.empty())
-        // {
-        //     TreeNode* node = q.front();
-        //     q.pop();
-        //     pq.push(node->val);
-        //     if(pq.size()>k) pq.pop();
-
-        //     if(node->left) q.push(node->left);
-        //     if(node->right) q.push(node->right);
-        // }
-        // return pq.top();
     }
 };
