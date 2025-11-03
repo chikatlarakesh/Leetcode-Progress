b1a1cf3cf21d147fc4c957f6b2e6873cf7ee88cc
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-11-03 19:02:49 +0530

    Time: 10 ms (23.03%) | Memory: 109.5 MB (6.92%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = colors.length();
+        vector<pair<char,int>> arr;
+        arr.push_back({colors[0],neededTime[0]});
+        int minTime = 0;
+        for(int i=1;i<n;i++) {
+            char ch = arr.back().first;
+            int time = arr.back().second;
+            if(colors[i] == ch) {
+                if(time < neededTime[i]) {
+                    minTime += time;
+                    arr.back().second = neededTime[i];
+                }
+                else minTime += neededTime[i];
+            }
+            else arr.push_back({colors[i],neededTime[i]});
+        }
+        return minTime;
+    }
+};
