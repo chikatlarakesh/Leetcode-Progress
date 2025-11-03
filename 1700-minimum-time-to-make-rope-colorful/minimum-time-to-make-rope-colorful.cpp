6a67adcc3c7619bf36113bda02e6001281e46e14
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-11-03 20:16:33 +0530

    Time: 4 ms (59.28%) | Memory: 99.9 MB (35.11%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -2,21 +2,34 @@ class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
         int n = colors.length();
-        vector<pair<char,int>> arr;
-        arr.push_back({colors[0],neededTime[0]});
         int minTime = 0;
+        int prev = neededTime[0];
         for(int i=1;i<n;i++) {
-            char ch = arr.back().first;
-            int time = arr.back().second;
-            if(colors[i] == ch) {
-                if(time < neededTime[i]) {
-                    minTime += time;
-                    arr.back().second = neededTime[i];
-                }
-                else minTime += neededTime[i];
+            int curr = neededTime[i];
+            if(colors[i] != colors[i-1]) {
+                prev = 0;
             }
-            else arr.push_back({colors[i],neededTime[i]});
+            minTime += min(prev,curr);
+            prev = max(prev,curr);
         }
         return minTime;
+
+
+        // vector<pair<char,int>> arr;
+        // arr.push_back({colors[0],neededTime[0]});
+        // int minTime = 0;
+        // for(int i=1;i<n;i++) {
+        //     char ch = arr.back().first;
+        //     int time = arr.back().second;
+        //     if(colors[i] == ch) {
+        //         if(time < neededTime[i]) {
+        //             minTime += time;
+        //             arr.back().second = neededTime[i];
+        //         }
+        //         else minTime += neededTime[i];
+        //     }
+        //     else arr.push_back({colors[i],neededTime[i]});
+        // }
+        // return minTime;
     }
 };
