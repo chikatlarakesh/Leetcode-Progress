cbafc1c974baec938e9e33d5d9a62f4f1db112a2
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-11-10 10:17:58 +0530

    Time: 50 ms (11.11%) | Memory: 39.5 MB (16.67%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -4,10 +4,9 @@ public:
         int n = nums.size();
         int count = 0;
         for(int i=0;i<n;i++) {
-            int maxValue = 0,freq = 0;
+            int freq = 0;
             for(int j=i;j<n;j++) {
                 if(nums[j] == target) freq++;
-                maxValue = max(maxValue,nums[j]);
                 int size = j - i + 1;
                 if(freq > (size / 2)) count++;
             }
