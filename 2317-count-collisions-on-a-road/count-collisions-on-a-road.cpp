b68726140733af83a153f1b855b3fb7b4be02a44
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2025-12-05 12:55:34 +0530

    Time: 10 ms (67.84%) | Memory: 19.2 MB (63.45%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.size();
+        int i = 0, j = n-1, count = 0;;
+        while(i < n && directions[i] == 'L') i++;
+        while(j >= 0 && directions[j] == 'R') j--;
+
+        while(i <= j) {
+            if(directions[i] != 'S') count++;
+            i++;
+        }
+        return count;
+    }
+};
