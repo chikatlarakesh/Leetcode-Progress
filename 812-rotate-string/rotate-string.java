88b85c853ff844af023acb9cf0c8f912e78e6b54
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2026-08-29 17:17:29 +0530

    Time: 2 ms (65.03%) | Memory: 43.2 MB (92.36%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        StringBuilder sb = new StringBuilder(s);
-        // same string twice
-        sb.append(s);
-
-        int n = s.length();
-        int len = n + n;
-        int i = 0, j = n - 1;
-        while(i < len && j < len) {
-            StringBuilder str = new StringBuilder(sb.substring(i,j+1));
-            if(goal.equals(str.toString())) return true;
-            i++;
-            j++;
-        }
-        return false;
+        return s.length() == goal.length() && (s + s).contains(goal);
     }
 }
