6ddac0997d1b7b685813bfb523e724099b081dfd
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2026-08-30 09:24:17 +0530

    Time: 3 ms (61.82%) | Memory: 86.8 MB (23.09%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -2,47 +2,66 @@ class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
         int minIndex = 0, maxIndex = 0;
-        int maxElement = nums[0], minElement = nums[0];
-
+    
         for(int i=1;i<n;i++) {
-            if(nums[i] > maxElement) {
-                maxElement = nums[i];
-                maxIndex = i;
-            }
-            if(nums[i] < minElement) {
-                minElement = nums[i];
-                minIndex = i;
-            }
+            if(nums[i] < nums[minIndex]) minIndex = i;
+            if(nums[i] > nums[maxIndex]) maxIndex = i;
         }
 
-        int minDeletions = n;
+        int left = Math.min(minIndex,maxIndex);
+        int right = Math.max(minIndex,maxIndex);
 
-        if(minIndex < maxIndex) {
-            int left = minIndex + 1;
-            int right = n - maxIndex;
-            minDeletions = Math.min(minDeletions,left+right);
+        int front = right + 1;
+        int back = n - left;
+        int frontAndBack = (left + 1) + (n - right);
 
-            int bothLeft = maxIndex + 1;
-            int bothRight = n - minIndex;
-            minDeletions = Math.min(minDeletions,bothLeft);
-            minDeletions = Math.min(minDeletions,bothRight);
-        }
-        else if(maxIndex < minIndex) {
-            int left = maxIndex + 1;
-            int right = n - minIndex;
-            minDeletions = Math.min(minDeletions,left+right);
+        int result = Math.min(frontAndBack,Math.min(front,back));
+        return result;
 
-            int bothLeft = minIndex + 1;
-            int bothRight = n - maxIndex;
-            minDeletions = Math.min(minDeletions,bothLeft);
-            minDeletions = Math.min(minDeletions,bothRight);
-        }
-        else {
-            int left = maxIndex + 1;
-            int right = n - minIndex;
-            minDeletions = Math.min(minDeletions,Math.min(left,right));
-        }
 
-        return minDeletions;
+        // int n = nums.length;
+        // int minIndex = 0, maxIndex = 0;
+        // int maxElement = nums[0], minElement = nums[0];
+
+        // for(int i=1;i<n;i++) {
+        //     if(nums[i] > maxElement) {
+        //         maxElement = nums[i];
+        //         maxIndex = i;
+        //     }
+        //     if(nums[i] < minElement) {
+        //         minElement = nums[i];
+        //         minIndex = i;
+        //     }
+        // }
+
+        // int minDeletions = n;
+
+        // if(minIndex < maxIndex) {
+        //     int left = minIndex + 1;
+        //     int right = n - maxIndex;
+        //     minDeletions = Math.min(minDeletions,left+right);
+
+        //     int bothLeft = maxIndex + 1;
+        //     int bothRight = n - minIndex;
+        //     minDeletions = Math.min(minDeletions,bothLeft);
+        //     minDeletions = Math.min(minDeletions,bothRight);
+        // }
+        // else if(maxIndex < minIndex) {
+        //     int left = maxIndex + 1;
+        //     int right = n - minIndex;
+        //     minDeletions = Math.min(minDeletions,left+right);
+
+        //     int bothLeft = minIndex + 1;
+        //     int bothRight = n - maxIndex;
+        //     minDeletions = Math.min(minDeletions,bothLeft);
+        //     minDeletions = Math.min(minDeletions,bothRight);
+        // }
+        // else {
+        //     int left = maxIndex + 1;
+        //     int right = n - minIndex;
+        //     minDeletions = Math.min(minDeletions,Math.min(left,right));
+        // }
+
+        // return minDeletions;
     }
 }
