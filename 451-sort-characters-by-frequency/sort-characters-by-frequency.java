6b4c6af22221649582988be59480b0ae58c866a0
Rakesh Chikatla <rakeshchikatla12@gmail.com>
2026-08-31 14:43:05 +0530

    Time: 6 ms (92.25%) | Memory: 46.2 MB (97.24%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,26 +1,41 @@
 class Solution {
+    class Pair {
+        int freq;
+        char ch;
+
+        public Pair(int freq,char ch) {
+            this.freq = freq;
+            this.ch = ch;
+        }
+    }
+
     public String frequencySort(String s) {
+        int n = s.length();
+        Pair[] hash = new Pair[256];
 
-        Map<Character,Long> mp = s.chars()
-                                    .mapToObj(c -> (char)c)
-                                    .collect(Collectors.groupingBy(
-                                        n -> n,
-                                        Collectors.counting()
-                                    ));
-        
-        return s.chars()
-                .mapToObj(c -> (char)c)
-                .sorted((ch1, ch2) -> {
-                    long freq1 = mp.get(ch1);
-                    long freq2 = mp.get(ch2);
+        for(int i=0;i<256;i++) {
+            hash[i] = new Pair(0,(char)(i));
+        }
 
-                    if (freq1 != freq2) {
-                        return Long.compare(freq2, freq1);
-                    }
+        for(char ch : s.toCharArray()) {
+            hash[ch].freq++;
+        }
 
-                    return Character.compare(ch1, ch2);
-                })
-                .map(String::valueOf)
-                .collect(Collectors.joining());
+        Arrays.sort(hash,(a,b) -> {
+            if(a.freq != b.freq) return b.freq - a.freq;
+            else return a.ch - b.ch;
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<256;i++) {
+            int freq = hash[i].freq;
+            if(freq == 0) break;
+            char ch = hash[i].ch;
+            while(freq > 0) {
+                sb.append(ch);
+                freq--;
+            }
+        }
+        return sb.toString();
     }
 }
